c35b7117b70fc14c8c9f58de6815f4a20fadb1a2
pekpuglia <pepuglia@gmail.com>
2025-04-07 11:05:55 +0200

    minimal comprehensible plot

--- src/sattoolboxtest.jl
+++ src/sattoolboxtest.jl
@@ -1,16 +1,35 @@
 using SatelliteToolboxBase
 using SatelliteToolboxPropagators
 using ForwardDiff
+using GLMakie
+using Setfield
 ##
 orb = KeplerianElements(
                   date_to_jd(2023, 1, 1, 0, 0, 0),
                   7190.982e3,
                   0.001111,
                   98.405 |> deg2rad,
-                  100    |> deg2rad,
+                  0    |> deg2rad,
                   90     |> deg2rad,
                   19     |> deg2rad
 )
+##
+N = 100
+θ = LinRange(0, 2π, N)
+
+orbit = ((@set orb.f = theta) for theta in θ) .|> kepler_to_rv .|> first |> stack
+current_pos, current_velocity = kepler_to_rv(orb)
+velocity_arrow_base = current_pos
+velocity_arrow_tip = velocity_arrow_base + current_velocity / √sum(current_velocity .^ 2) * 0.4 * √sum(current_pos .^ 2)
+velocity_arrow_data = [velocity_arrow_base velocity_arrow_tip] |> Matrix
+##
+fig = Figure()
+ax3d = Axis3(fig[1, 1])
+lines!(ax3d, orbit[1, :], orbit[2, :], orbit[3, :], color=:red)
+wireframe!(ax3d, Sphere(Point3(0.0), EARTH_EQUATORIAL_RADIUS), color=:cyan, alpha=0.3)
+scatter!(ax3d, current_pos, markersize=20)
+lines!(ax3d, velocity_arrow_data[1, :], velocity_arrow_data[2, :], velocity_arrow_data[3, :])
+fig
 ##
 orbp = Propagators.init(Val(:TwoBody), orb)
 ##
