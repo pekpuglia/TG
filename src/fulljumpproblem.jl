17713cff6d2d0d3acf0315655a152f4924b40046
pekpuglia <pepuglia@gmail.com>
2025-04-08 12:13:44 +0200

    rollback entire file

--- src/fulljumpproblem.jl
+++ src/fulljumpproblem.jl
@@ -6,11 +6,8 @@ using Setfield
 using ForwardDiff
 include("TG.jl")
 using .TG
-## ######################################################
-#       designing single maneuver inversely
-#
-#########################################################
-
+##
+#designing single maneuver inversely
 orb0 = KeplerianElements(
                   date_to_jd(2023, 1, 1, 0, 0, 0),
                   7190.982e3,
@@ -21,7 +18,6 @@ orb0 = KeplerianElements(
                   0.5     |> deg2rad
 )
 T0 = orbital_period(orb0, tbc_m0)
-r0, v0 = kepler_to_rv(orb0)
 ##
 r_preman, v_preman, orbp_preman = Propagators.propagate(Val(:TwoBody), T0/4, orb0)
 orb_preman = orbp_preman.tbd.orbk
@@ -33,17 +29,71 @@ plot_orbit(orb0, orb_postman)
 T_postman = orbital_period(orb_postman, tbc_m0)
 r_final, v_final, orbp_postman = Propagators.propagate(Val(:TwoBody), 0.3*T_postman, orb_postman)
 orb_final = orbp_postman.tbd.orbk
-# SatelliteToolbox stores time as Julian Day
-episode_time = (orb_final.t - orb0.t) * 2π/EARTH_ANGULAR_SPEED
 plot_orbit(orb_final)
+## optimize [t; ΔV] for this maneuver
+noNaNs(x::Real) = true
+noNaNs(x::ForwardDiff.Dual) = !any(isnan, ForwardDiff.partials(x))
+function final_state(maneuver_params, prob_params)
+    orb0       = prob_params[1]
+    total_time = prob_params[2]
+
+    #normalized time
+    maneuver_time = maneuver_params[1] * total_time
+    maneuver_deltaV = maneuver_params[2:4]
+
+
+    propagator_preman = Propagators.init(Val(:TwoBody), orb0, propagation_type=Real)
+
+    r_preman, v_preman = Propagators.propagate!(propagator_preman, maneuver_time)
+    v_postman = v_preman + maneuver_deltaV
+
+    @assert noNaNs(r_preman[1]) "NaN found"
+    @assert noNaNs(r_preman[2]) "NaN found"
+    @assert noNaNs(r_preman[3]) "NaN found"
+    @assert noNaNs(v_postman[1]) "NaN found"
+    @assert noNaNs(v_postman[2]) "NaN found"
+    @assert noNaNs(v_postman[3]) "NaN found"
+
+    orb_postman = rv_to_kepler(r_preman, v_postman, propagator_preman.tbd.orbk.t)
+    
+    @assert noNaNs(orb_postman.a) "NaN found"
+    @assert noNaNs(orb_postman.e) "NaN found"
+    @assert noNaNs(orb_postman.f) "NaN found"
+    @assert noNaNs(orb_postman.i) "NaN found"
+    @assert noNaNs(orb_postman.t) "NaN found"
+    @assert noNaNs(orb_postman.Ω) "NaN found"
+    @assert noNaNs(orb_postman.ω) "NaN found"
+
+    propagator_postman = Propagators.init(Val(:TwoBody), orb_postman, propagation_type=Real)
+
+    r_final, v_final = Propagators.propagate!(propagator_postman, total_time-maneuver_time)
+    
+    [r_final; v_final]
+end
+##
+prob_params = [
+    orb0,
+    T0/4+0.3*T_postman,
+    r_final, v_final
+]
 ##
 prob_answer = [
-    T0/4 / (T0/4+0.3*T_postman);
+    T0/4 / (prob_params[2]);
     deltaV
 ]
-#####################################################################################
-## optimize [t; ΔV] for this maneuver - put into TG module
-
+##
+final_state(prob_answer, prob_params)
+##
+man0 = [
+    0.0
+    0.0
+    0.0
+    0.0
+]
+##
+final_state(man0, prob_params)
+##
+ForwardDiff.jacobian(m -> final_state(m, prob_params), man0)
 ## https://jump.dev/JuMP.jl/stable/tutorials/nonlinear/tips_and_tricks/#User-defined-operators-with-vector-outputs
 """
     memoize(foo::Function, n_outputs::Int)
@@ -73,42 +123,10 @@ function memoize(foo::Function, n_outputs::Int)
     end
     return [(x...) -> foo_i(i, x...) for i in 1:n_outputs]
 end
-
-noNaNs(x::Real) = true
-noNaNs(x::ForwardDiff.Dual) = !any(isnan, ForwardDiff.partials(x))
-
-function propagate_state(x0, y0, z0, vx0, vy0, vz0, t0, Δt)
-    orb0 = rv_to_kepler([x0;y0;z0], [vx0; vy0; vz0], t0)
-
-    @assert noNaNs(orb0.a) "NaN found"
-    @assert noNaNs(orb0.e) "NaN found"
-    @assert noNaNs(orb0.f) "NaN found"
-    @assert noNaNs(orb0.i) "NaN found"
-    @assert noNaNs(orb0.t) "NaN found"
-    @assert noNaNs(orb0.Ω) "NaN found"
-    @assert noNaNs(orb0.ω) "NaN found"
-
-    propagator0 = Propagators.init(Val(:TwoBody), orb0, propagation_type=Real)
-
-    rf, vf = Propagators.propagate!(propagator0, Δt)
-    
-    @assert noNaNs(rf[1]) "NaN found"
-    @assert noNaNs(rf[2]) "NaN found"
-    @assert noNaNs(rf[3]) "NaN found"
-    @assert noNaNs(vf[1]) "NaN found"
-    @assert noNaNs(vf[2]) "NaN found"
-    @assert noNaNs(vf[3]) "NaN found"
-
-    [rf; vf; propagator0.tbd.orbk.t]
-end
+memoized_final_state = memoize(
+    (t, dVx, dVy, dVz) -> final_state([t;dVx;dVy;dVz], prob_params), 6)
 ##
-propagate_state(r_preman..., v_postman..., orb_preman.t, 0.3*T_postman)
-##
-ForwardDiff.jacobian(x -> propagate_state(x..., 0.3*T_postman), [r_preman..., v_postman..., orb_preman.t])
-##
-memoized_propagate_state = memoize(propagate_state, 7)
-##
-[f(r_preman..., v_postman..., orb_preman.t, 0.3*T_postman) for f in memoized_propagate_state]
+memoized_final_state[1](man0...)
 ##
 function residuals(x, y, z, vx, vy, vz, r_target, v_target)
     r_norm = √sum(r_target' * r_target)
@@ -122,51 +140,27 @@ function residuals(x, y, z, vx, vy, vz, r_target, v_target)
 end
 ##
 model = Model(Ipopt.Optimizer)
+@variable(model, t_maneuver, start=0.0)
+@variable(model, ΔV[i = 1:3], start=0.0)
 
-propagated_position_x = @operator(model, propagated_position_x, 8, memoized_propagate_state[1])
-propagated_position_y = @operator(model, propagated_position_y, 8, memoized_propagate_state[2])
-propagated_position_z = @operator(model, propagated_position_z, 8, memoized_propagate_state[3])
-propagated_velocity_x = @operator(model, propagated_velocity_x, 8, memoized_propagate_state[4])
-propagated_velocity_y = @operator(model, propagated_velocity_y, 8, memoized_propagate_state[5])
-propagated_velocity_z = @operator(model, propagated_velocity_z, 8, memoized_propagate_state[6])
-propagated_time       = @operator(model, propagated_time,       8, memoized_propagate_state[7])
-
-Δt_maneuver = @variable(model, Δt_maneuver, start=0.0)
-ΔV         = @variable(model, ΔV[i = 1:3], start=0.0)
-
-x_premaneuver  = propagated_position_x(r0..., v0..., orb0.t, Δt_maneuver)
-y_premaneuver  = propagated_position_y(r0..., v0..., orb0.t, Δt_maneuver)
-z_premaneuver  = propagated_position_z(r0..., v0..., orb0.t, Δt_maneuver)
-vx_premaneuver = propagated_velocity_x(r0..., v0..., orb0.t, Δt_maneuver)
-vy_premaneuver = propagated_velocity_y(r0..., v0..., orb0.t, Δt_maneuver)
-vz_premaneuver = propagated_velocity_z(r0..., v0..., orb0.t, Δt_maneuver)
-t_maneuver     = propagated_time(      r0..., v0..., orb0.t, Δt_maneuver)
-
-vx_postmaneuver = vx_premaneuver + ΔV[1]
-vy_postmaneuver = vy_premaneuver + ΔV[2]
-vz_postmaneuver = vz_premaneuver + ΔV[3]
-
-r_premaneuver  = [x_premaneuver  ; y_premaneuver  ; z_premaneuver]
-v_postmaneuver = [vx_postmaneuver; vy_postmaneuver; vz_postmaneuver]
-
-x_final  = propagated_position_x(r_premaneuver..., v_postmaneuver..., t_maneuver, episode_time - Δt_maneuver)
-y_final  = propagated_position_y(r_premaneuver..., v_postmaneuver..., t_maneuver, episode_time - Δt_maneuver)
-z_final  = propagated_position_z(r_premaneuver..., v_postmaneuver..., t_maneuver, episode_time - Δt_maneuver)
-vx_final = propagated_velocity_x(r_premaneuver..., v_postmaneuver..., t_maneuver, episode_time - Δt_maneuver)
-vy_final = propagated_velocity_y(r_premaneuver..., v_postmaneuver..., t_maneuver, episode_time - Δt_maneuver)
-vz_final = propagated_velocity_z(r_premaneuver..., v_postmaneuver..., t_maneuver, episode_time - Δt_maneuver)
+@operator(model, final_position_x, 4, memoized_final_state[1])
+@operator(model, final_position_y, 4, memoized_final_state[2])
+@operator(model, final_position_z, 4, memoized_final_state[3])
+@operator(model, final_velocity_x, 4, memoized_final_state[4])
+@operator(model, final_velocity_y, 4, memoized_final_state[5])
+@operator(model, final_velocity_z, 4, memoized_final_state[6])
 
 @constraint(model, 0 <= t_maneuver <= 1)
 @constraint(model, 0 <= ΔV' * ΔV <= 2e6)
 
-@objective(model, MIN_SENSE, residuals(
-    x_final,
-    y_final,
-    z_final,
-    vx_final,
-    vy_final,
-    vz_final,
-    r_final, v_final
+@objective(model, Min, residuals(
+    final_position_x(t_maneuver, ΔV[1], ΔV[2], ΔV[3]),
+    final_position_y(t_maneuver, ΔV[1], ΔV[2], ΔV[3]),
+    final_position_z(t_maneuver, ΔV[1], ΔV[2], ΔV[3]),
+    final_velocity_x(t_maneuver, ΔV[1], ΔV[2], ΔV[3]),
+    final_velocity_y(t_maneuver, ΔV[1], ΔV[2], ΔV[3]),
+    final_velocity_z(t_maneuver, ΔV[1], ΔV[2], ΔV[3]),
+    prob_params[3], prob_params[4]
 ))
 # @constraint(model, final_state([t_maneuver; ΔV], prob_params) == [prob_params[3]; prob_params[4]])
 model
@@ -174,4 +168,3 @@ model
 optimize!(model)
 ##
 value.(all_variables(model))
-##
