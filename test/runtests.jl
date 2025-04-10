76ad0903822044a87b1d3f8c52018c28d5e66c9f
pekpuglia <pepuglia@gmail.com>
2025-04-10 11:06:45 +0200

    geo test added

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,25 +11,59 @@ using SatelliteToolboxBase
 #
 ##########################################################################
 @testset "Single Maneuver Models" begin
-    LEOorb = KeplerianElements(
-                  date_to_jd(2023, 1, 1, 0, 0, 0),
-                  8000e3,
-                  0.101111,
-                  20 |> deg2rad,
-                  50    |> deg2rad,
-                  30     |> deg2rad,
-                  70     |> deg2rad
-    )
+    @testset "LEO case" begin
+        LEOorb = KeplerianElements(
+                      date_to_jd(2023, 1, 1, 0, 0, 0),
+                      8000e3,
+                      0.101111,
+                      20 |> deg2rad,
+                      50    |> deg2rad,
+                      30     |> deg2rad,
+                      70     |> deg2rad
+        )
+    
+        LEOdeltaV = [1000, 0, 0]
+    
+        LEO_r_final, LEO_total_time, _, _ = final_position(LEOorb, LEOdeltaV, 0.25, 0.3)
+    
+        model, r_maneuver, v_post_maneuver, rf, vf = single_maneuver_model(LEOorb, LEO_r_final, LEO_total_time);
+    
+        optimize!(model)
+    
+        @test is_solved_and_feasible(model)
+    
+        @test norm(value.(model[:ΔV])) <= norm(LEOdeltaV)
+    end
 
-    LEOdeltaV = [1000, 0, 0]
-
-    LEO_r_final, LEO_total_time, _, _ = final_position(LEOorb, LEOdeltaV, 0.25, 0.3)
-
-    model, r_maneuver, v_post_maneuver, rf, vf = single_maneuver_model(LEOorb, LEO_r_final, LEO_total_time);
-
-    optimize!(model)
-
-    @test is_solved_and_feasible(model)
-
-    @test norm(value.(model[:ΔV])) <= norm(LEOdeltaV)
+    @testset "GEO case" begin
+        orb = KeplerianElements(
+                      date_to_jd(2023, 1, 1, 0, 0, 0),
+                      30000e3,
+                      0.101111,
+                      20 |> deg2rad,
+                      50    |> deg2rad,
+                      30     |> deg2rad,
+                      70     |> deg2rad
+        )
+    
+        deltaV = [-1000, 0, 0]
+    
+        r_final, total_time, _, _ = final_position(
+            orb, 
+            deltaV,
+            0.25,
+            0.3)
+    
+        model, r_maneuver, v_post_maneuver, rf, vf = single_maneuver_model(
+            orb, 
+            r_final, 
+            total_time);
+    
+        optimize!(model)
+    
+        @test is_solved_and_feasible(model)
+    
+        @test norm(value.(model[:ΔV])) <= norm(deltaV)
+    end
+    
 end
