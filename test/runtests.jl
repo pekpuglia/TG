adae65f8c1a756b2d90c2b4438ae49d95ae32534
pekpuglia <pepuglia@gmail.com>
2025-04-10 10:44:20 +0200

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,35 @@
 using Test
-using Mosek
-using MosekTools
+include("../src/TG.jl")
+using .TG
+using JuMP
+using LinearAlgebra
+using SatelliteToolboxBase
 
-########################
-# test mosek installation
-@test begin
-    x = maketask() do task
-        # Use remote server: putoptserverhost(task,"http://solve.mosek.com:30080")
-        appendvars(task, 1)                             # 1 variable x
-        putcj(task, 1, 1.0)                             # c_0 = 1.0
-        putvarbound(task, 1, MSK_BK_RA, 2.0, 3.0)       # 2.0 <= x <= 3.0
-        putobjsense(task, MSK_OBJECTIVE_SENSE_MINIMIZE) # minimize
-    
-        optimize(task)                                  # Optimize
-    
-        getxx(task, MSK_SOL_ITR)                    # Get solution
-    end
-    x == [2.0]
+##########################################################################
+#
+#       Single Maneuver Tests
+#
+##########################################################################
+@testset "Single Maneuver Models" begin
+    LEOorb = KeplerianElements(
+                  date_to_jd(2023, 1, 1, 0, 0, 0),
+                  8000e3,
+                  0.101111,
+                  20 |> deg2rad,
+                  50    |> deg2rad,
+                  30     |> deg2rad,
+                  70     |> deg2rad
+    )
+
+    LEOdeltaV = [1000, 0, 0]
+
+    LEO_r_final, LEO_total_time, _, _ = final_position(LEOorb, LEOdeltaV, 0.25, 0.3)
+
+    model, r_maneuver, v_post_maneuver, rf, vf = single_maneuver_model(LEOorb, LEO_r_final, LEO_total_time);
+
+    optimize!(model)
+
+    @test is_solved_and_feasible(model)
+
+    @test norm(value.(model[:ΔV])) < norm(LEOdeltaV)
 end
