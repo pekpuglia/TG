af5a78b83298f7b5a12bb68f9ce49308b4eda02d
pekpuglia <pepuglia@gmail.com>
2025-04-11 16:10:02 +0200

    added tolerances, everything works even with energy > 0

--- src/jump_propagation_sandbox.jl
+++ src/jump_propagation_sandbox.jl
@@ -13,14 +13,15 @@ orb0 = KeplerianElements(
     date_to_jd(2023, 1, 1, 0, 0, 0),
     7000e3,
     0.01,
-    1.5 |> deg2rad,
+    91.5 |> deg2rad,
     91.5    |> deg2rad,
     271.5     |> deg2rad,
-    1.5     |> deg2rad
+    263     |> deg2rad
 )
 r0, v0 = kepler_to_rv(orb0)
-T0 = orbital_period(orb0, GM_EARTH)
-# plot_orbit(orb0)
+v0 = v0 * 10/7
+# T0 = orbital_period(orb0, GM_EARTH)
+plot_orbit(rv_to_kepler(r0, v0, orb0.t))
 ## CURTIS chap 3
 
 model = Model(
@@ -30,8 +31,6 @@ model = Model(
 )
 
 @variables(model, begin
-    a
-    e
     0 <= id <= 180
     Ω
     ω
@@ -39,36 +38,45 @@ model = Model(
 end)
 # @variable(model, r[1:3])
 # @variable(model, v[1:3])
-
+tol = 1e-3
 rnorm = norm(r0)
 vnorm = norm(v0)
 vr = dot(r0/rnorm, v0)
 
 h = cross(r0, v0)
 
-@constraint(model, cosd(id) == h[3]/√(h' * h))
+normal_direction = h / √(h' * h)
+
+@constraint(model, -tol <= cosd(id) - normal_direction[3] <= tol)
 
 N = cross([0;0;1], h)
 
 Nnorm = √(N' * N)
 
-@constraint(model, cosd(Ω) == N[1]/Nnorm)
-@constraint(model, sind(Ω) == N[2]/Nnorm)
+@constraint(model, -tol <= cosd(Ω) - N[1]/Nnorm <= tol)
+@constraint(model, -tol <= sind(Ω) - N[2]/Nnorm <= tol)
 
 exc_vec = (vnorm^2 / GM_EARTH - 1 / rnorm) * r0 - rnorm*vr/GM_EARTH .* v0
 
 e = √(exc_vec' * exc_vec)
 
-@constraint(model, cosd(ω) == dot(N, exc_vec) / (Nnorm*e))
+@constraint(model, -tol <= cosd(ω) - dot(N, exc_vec) / (Nnorm*e) <= tol)
 
 N_e_cross = cross(N, exc_vec)
-normal_N_e_cross = dot(N_e_cross, h/√(h' * h))
+normal_N_e_cross = dot(N_e_cross, normal_direction)
 
-@constraint(model, sind(ω) == normal_N_e_cross / (Nnorm*e))
+@constraint(model, -tol <= sind(ω) - normal_N_e_cross / (Nnorm*e) <= tol)
+
+@constraint(model, -tol <= cosd(nu) - dot(exc_vec, r0) / (e*rnorm) <= tol)
+
+exc_r_cross = cross(exc_vec, r0)
+normal_exc_r_cross = dot(exc_r_cross, normal_direction)
+@constraint(model, -tol <= sind(nu) -  normal_exc_r_cross / (e*rnorm) <= tol)
 
 model
 
 ##
 optimize!(model)
-value(model[:ω])
 value.(all_variables(model))
+##
+value(e)
