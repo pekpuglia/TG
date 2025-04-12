bd1b17a8fcfe3a6fb14c6bbecdfca93241c8c30d
pekpuglia <pepuglia@gmail.com>
2025-04-12 17:09:35 +0200

    start fix example 3.2

--- src/jump_propagation_sandbox.jl
+++ src/jump_propagation_sandbox.jl
@@ -8,100 +8,86 @@ include("TG.jl")
 using .TG
 using LinearAlgebra
 ##
-#designing single maneuver inversely
+function add_orbital_elements_fix!(model)
+    Vorb_sup = √(GM_EARTH/EARTH_EQUATORIAL_RADIUS)
+    r = @variable(model, [1:3], start = EARTH_EQUATORIAL_RADIUS)
+    v = @variable(model, [1:3])
+    set_start_value(v[1], Vorb_sup)
+
+    #adding exc as variable so bounds will always be respected
+    #then need to put constraint on it and implement E and M
+    #deg!!!
+    e = @variable(model, lower_bound = 0, upper_bound = 1) 
+    i = @variable(model, lower_bound = 0, upper_bound = 180, base_name = "i")
+    Ω = @variable(model, base_name = "Ω")
+    ω = @variable(model, base_name = "ω")
+    nu = @variable(model, base_name = "nu")
+
+    #rad!!!
+    M = @variable(model, base_name = "M")
+    E = @variable(model, base_name= "E")
+    
+    tol = 1e-9
+    rnorm = √(r' * r)
+    vnorm = √(v' * v)
+    
+    a = -1 / (- 2 / rnorm + vnorm^2 / GM_EARTH)
+    
+    vr = dot(r ./ rnorm, v)
+    
+    h = cross(r, v)
+    
+    normal_direction = h ./ √(h' * h)
+    
+    @constraint(model, cosd(i) == normal_direction[3])
+    
+    N = cross([0;0;1], h)
+    
+    Nnorm = √(N' * N)
+    
+    @constraint(model, cosd(Ω) == N[1]/Nnorm)
+    @constraint(model, -tol <= sind(Ω) - N[2]/Nnorm <= tol)
+    
+    exc_vec = (vnorm^2 / GM_EARTH - 1 / rnorm) * r - rnorm*vr/GM_EARTH .* v
+    
+    exc_vec_norm = √(exc_vec' * exc_vec)
+
+    @constraint(model, e == exc_vec_norm)
+    
+    @constraint(model, cosd(ω) == dot(N, exc_vec) / (Nnorm*exc_vec_norm))
+    
+    N_e_cross = cross(N, exc_vec)
+    normal_N_e_cross = dot(N_e_cross, normal_direction)
+    
+    @constraint(model, -tol <= sind(ω) - normal_N_e_cross / (Nnorm*exc_vec_norm) <= tol)
+    
+    @constraint(model, cosd(nu) == dot(exc_vec, r) / (exc_vec_norm*rnorm))
+    
+    exc_r_cross = cross(exc_vec, r)
+    normal_exc_r_cross = dot(exc_r_cross, normal_direction)
+    @constraint(model, -tol <= sind(nu) -  normal_exc_r_cross / (exc_vec_norm*rnorm) <= tol)
+    
+    @constraint(model, E - e*sin(E) == M)
+
+    #curtis page 144 & 145
+    @constraint(model, cos(E) == (e + cosd(nu)) / (1 + e*cosd(nu)))
+    @constraint(model, -tol <= sin(E) - √(1-e^2)*sind(nu) / (1 + e*cosd(nu)) <= tol)
+
+    r, v, a, e, i, Ω, ω, nu, M, E
+end
+## example 3.2
+rp = 9600e3
+ra = 21000e3
+a = (rp + ra) / 2
+e = (ra - rp) / (ra + rp)
 orb0 = KeplerianElements(
     date_to_jd(2023, 1, 1, 0, 0, 0),
-    7000e3,
-    0.01,
-    1.5 |> deg2rad,
-    1.5    |> deg2rad,
-    1.5     |> deg2rad,
-    1.5     |> deg2rad
+    a,
+    e,
+    30 |> deg2rad,
+    0    |> deg2rad,
+    0     |> deg2rad,
+    0     |> deg2rad
 )
 r0, v0 = kepler_to_rv(orb0)
-
-deltaV = [-1000, 0, 0]
-
-r_final, total_time, orb_postman, orb_final, v_final = final_position(
-    orb0, 
-    deltaV, 
-    0.2, 
-    0.5
-)
-
-plot_orbit(orb0, orb_postman, orb_final)
-## solving maneuver
-# model, r_maneuver, v_post_maneuver, rf, vf = single_maneuver_model(
-#     orb0, 
-#     r_final, 
-#     dir_v_final,
-#     total_time);
-# set_silent(model)
-model = Model(
-    optimizer_with_attributes(Ipopt.Optimizer,
-        "max_iter" => 10000,
-        "max_wall_time" => 30.0)
-)
-
-Δt_maneuver = @variable(model, Δt_maneuver, start=0.5*total_time)
-@constraint(model, 0 <= Δt_maneuver <= total_time)  #set start value of constraints?
-
-ΔVmag = @variable(model, 0 <= ΔVmag, start=1.0)
-    
-ΔVdir = @variable(model, -1 <= ΔVdir[1:3] <= 1, start = 0.0)
-set_start_value(ΔVdir[1], 1.0)
-
-@constraint(model, ΔVdir' * ΔVdir == 1)
-
-orbparams_i = add_orbital_elements!(model)
-
-orbparams_pre_maneuver = add_orbital_elements!(model)
-orbparams_post_maneuver = add_orbital_elements!(model)
-
-orbparams_f = add_orbital_elements!(model)
-
-#model evolution
-
-#starting condition
-@constraint(model, orbparams_i.r .== r0)
-@constraint(model, orbparams_i.v .== v0)
-
-#coast 1
-Tpreman = orbital_period(orbparams_i.a, GM_EARTH)
-
-@constraint(model, Δt_maneuver == (orbparams_pre_maneuver.M - orbparams_i.M) / (2π) * Tpreman)
-
-#maneuver
-@constraint(model, orbparams_post_maneuver.r == orbparams_pre_maneuver.r)
-@constraint(model, orbparams_post_maneuver.v == orbparams_pre_maneuver.v + ΔVmag * ΔVdir)
-
-#coast 2
-Tpostman = orbital_period(orbparams_post_maneuver.a, GM_EARTH)
-@constraint(model, total_time - Δt_maneuver == (orbparams_f.M - orbparams_post_maneuver.M) / (2π) * Tpostman)
-
-#final conditions
-@constraint(model, orbparams_f.r .== r_final)
-# @constraint(model, orbparams_f.v .== v_final)
-
-@objective(model, MIN_SENSE, ΔVmag)
-
-model
-
-##
-optimize!(model)
-##
-solved_rf = value.(orbparams_f.r)
-solved_vf = value.(orbparams_f.v)
-
-solved_deltaV = value.(ΔVmag * ΔVdir)
-
-solved_r_maneuver = value.(orbparams_pre_maneuver.r)
-solved_v_post_maneuver = value.(orbparams_post_maneuver.v)
-
-plot_orbit(
-orb0, 
-rv_to_kepler(solved_r_maneuver, solved_v_post_maneuver), 
-orb_final, 
-rv_to_kepler(solved_rf, solved_vf)) |> display
-##
-objective_value(model), value(model[:Δt_maneuver])
+Δt = 3*3600.0
