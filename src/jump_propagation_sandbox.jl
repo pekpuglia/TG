6ca2552ae55e4a77cf385027d68e73b21d2cc5bb
pekpuglia <pepuglia@gmail.com>
2025-04-12 20:50:55 +0200

    nothing works

--- src/jump_propagation_sandbox.jl
+++ src/jump_propagation_sandbox.jl
@@ -7,43 +7,41 @@ using ForwardDiff
 include("TG.jl")
 using .TG
 using LinearAlgebra
-## example 3.1
-rp = 9600e3
-ra = 21000e3
-a = (rp + ra) / 2
-e = (ra - rp) / (ra + rp)
+## example 2.7
+rp = (6378+400)*1000.0
+ra = (6378+4000)*1000.0
+agiven = (rp + ra) / 2
+egiven = (ra - rp) / (ra + rp)
 orb0 = KeplerianElements(
     date_to_jd(2023, 1, 1, 0, 0, 0),
-    a,
-    e,
+    agiven,
+    egiven,
     30 |> deg2rad,
     0    |> deg2rad,
     0     |> deg2rad,
-    90     |> deg2rad
+    0     |> deg2rad
 )
 r0, v0 = kepler_to_rv(orb0)
-orbf = @set orb0.f = 120 |> deg2rad
-r, v = kepler_to_rv(orbf)
-plot_orbit(orb0, orbf)
+plot_orbit(orb0)
 ##
 function add_orbital_elements_fix!(model)
     Vorb_sup = √(GM_EARTH/EARTH_EQUATORIAL_RADIUS)
-    r = @variable(model, [1:3], start = EARTH_EQUATORIAL_RADIUS)
-    v = @variable(model, [1:3])
-    set_start_value(v[1], Vorb_sup)
+    rscaled = @variable(model, [1:3], start = 1.0)
+    r = EARTH_EQUATORIAL_RADIUS * rscaled
+    vscaled = @variable(model, [1:3])
+    set_start_value(vscaled[1], 1.0)
+    v = Vorb_sup*vscaled
 
-    #adding exc as variable so bounds will always be respected
-    #then need to put constraint on it and implement E and M
-    #deg!!!
+    a = @variable(model, lower_bound = 0, start = EARTH_EQUATORIAL_RADIUS)
     e = @variable(model, lower_bound = 0, upper_bound = 1) 
     i = @variable(model, lower_bound = 0, upper_bound = π, base_name = "i")
     Ω = @variable(model, base_name = "Ω")
     ω = @variable(model, base_name = "ω")
-    nu = @variable(model, base_name = "nu")
+    nu = @variable(model, start = π, base_name = "nu")
 
     #rad!!!
-    M = @variable(model, base_name = "M")
-    E = @variable(model, base_name= "E")
+    M = @variable(model, start = π, base_name = "M")
+    E = @variable(model, start = π, base_name= "E")
     
     hvec = cross(r, v)
     h = √(hvec' * hvec)
@@ -68,7 +66,7 @@ function add_orbital_elements_fix!(model)
     R3omega = [
         cos(Ω) -sin(Ω) 0
         sin(Ω)  cos(Ω) 0
-        0           0         1    
+        0           0         1
     ]
 
     QXxbar = R3omega * R1i * R3Omega
@@ -79,7 +77,7 @@ function add_orbital_elements_fix!(model)
     @constraint(model, E - e*sin(E) == M)
 
     #curtis page 144 & 145
-    @constraint(model, E == 2 * atan(√((1-e)/(1+e))*tand(nu/2)))
+    @constraint(model, E == 2 * atan(√((1-e)/(1+e))*tan(nu/2)))
 
     FullOrbitalParameters(r, v, a, e, i, Ω, ω, nu, M, E)
 end
@@ -89,30 +87,45 @@ model = Model(
     "max_wall_time" => 30.0)
 )
 orbparams_i = add_orbital_elements_fix!(model)
-ri, vi, ai, ei, ii, Ωi, ωi, nui, Mi, Ei = getfield.(Ref(orbparams_i), fieldnames(FullOrbitalParameters))
-orbparams_f = add_orbital_elements_fix!(model)
-rf, vf, af, ef, i_f, Ωf, ωf, nuf, Mf, Ef = getfield.(Ref(orbparams_f), fieldnames(FullOrbitalParameters))
+r, v, a, e, i, Ω, ω, nu, M, E = getfield.(Ref(orbparams_i), fieldnames(FullOrbitalParameters))
 
-@variable(model, Δt)
+# @constraint(model, a*(1-e) == rp)
+# @constraint(model, a*(1+e) == ra)
 
-@constraint(model, ri .== r0)
-@constraint(model, vi .== v0)
-@constraint(model, rf .== r)
-@constraint(model, vf' * vf .== v' * v)
+# @constraint(model, i == deg2rad(30))
 
-T = orbital_period(ai, GM_EARTH)
+# @constraint(model, nu == 0)
 
+# @constraint(model, Ω == 0)
 
-@constraint(model, Δt == (Mf - Mi) / (2π) * T)
+# @constraint(model, ω == 0)
+
+@constraint(model, r .== r0)
+@constraint(model, v .== v0)
+
 model
 ##
 optimize!(model)
 ##
-solved_ri = value.(ri)
-solved_vi = value.(vi)
-solved_rf = value.(rf)
-solved_vf = value.(vf)
+value(a), agiven
+##
+value(e), egiven
+##
+value(nu)
+##
+rad2deg(value(i))
+##
+value(Ω)
+##
+value(ω)
+##
+r0, value.(r)
+##
+v0, value.(v)
+##
+solved_r = value.(r)
+solved_v = value.(v)
 plot_orbit(
-    rv_to_kepler(solved_ri, solved_vi),
-    rv_to_kepler(solved_rf, solved_vf)
+    orb0,
+    rv_to_kepler(solved_r, solved_v),
 )
