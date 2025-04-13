59b502de337388d4b66c0bf56475e25acce08e9a
pekpuglia <pepuglia@gmail.com>
2025-04-13 17:37:58 +0200

    remove orbital elements fix function, all propagation tests pass

--- src/jump_propagation_sandbox.jl
+++ src/jump_propagation_sandbox.jl
@@ -7,94 +7,13 @@ using ForwardDiff
 include("TG.jl")
 using .TG
 using LinearAlgebra
-## example 2.7
-rp = (6378+400)*1000.0
-ra = (6378+4000)*1000.0
-agiven = (rp + ra) / 2
-egiven = (ra - rp) / (ra + rp)
-orb0 = KeplerianElements(
-    date_to_jd(2023, 1, 1, 0, 0, 0),
-    agiven,
-    egiven,
-    30 |> deg2rad,
-    0    |> deg2rad,
-    30     |> deg2rad,
-    180     |> deg2rad
-)
-r0, v0 = kepler_to_rv(orb0)
-plot_orbit(orb0)
-##
-#given rv chooses if r and v are constrained or if the constraints are
-#on the orbital elements
-function add_orbital_elements_fix!(model, given_rv = true)
-    Vorb_sup = √(GM_EARTH/EARTH_EQUATORIAL_RADIUS)
-    rscaled = @variable(model, [1:3], start = 1.0)
-    r = EARTH_EQUATORIAL_RADIUS * rscaled
-    vscaled = @variable(model, [1:3])
-    set_start_value(vscaled[1], 1.0)
-    v = Vorb_sup*vscaled
-
-    a = @variable(model, lower_bound = 0, start = EARTH_EQUATORIAL_RADIUS)
-    e = @variable(model, lower_bound = 0, upper_bound = 1) 
-    i = @variable(model, lower_bound = 0, upper_bound = π, base_name = "i")
-    Ω = @variable(model, base_name = "Ω")
-    ω = @variable(model, base_name = "ω")
-    nu = @variable(model, base_name = "nu")
-
-    #rad!!!
-    M = @variable(model, base_name = "M")
-    E = @variable(model, base_name= "E")
-    
-    R3Omega = [
-         cos(Ω) sin(Ω) 0
-        -sin(Ω) cos(Ω) 0
-        0          0        1
-    ]
-
-    R1i = [
-        1  0         0
-        0  cos(i) sin(i)
-        0 -sin(i) cos(i)
-    ]
-
-    R3omega = [
-        cos(ω)  sin(ω) 0
-        -sin(ω) cos(ω) 0
-        0          0        1
-    ]
-
-    QXxbar = R3omega * R1i * R3Omega
-
-    if given_rv
-        hvec = cross(r, v)
-        h = √(hvec' * hvec)
-    else
-        h = √(GM_EARTH*a*(1-e^2))
-    end
-
-    #curtis chap 4
-    #h^2/mu = p = a (1-e^2)
-    r_perifocal = a*(1-e^2) * 1/(1+e*cos(nu)) * [cos(nu); sin(nu); 0]
-
-    v_perifocal = GM_EARTH / h * [-sin(nu); e + cos(nu); 0]
-
-
-    @constraint(model, r .== QXxbar' * r_perifocal)
-    @constraint(model, v .== QXxbar' * v_perifocal)
-    
-    @constraint(model, E - e*sin(E) == M)
-
-    #curtis page 144 & 145
-    @constraint(model, E == 2 * atan(√((1-e)/(1+e))*tan(nu/2)))
-
-    FullOrbitalParameters(r, v, a, e, i, Ω, ω, nu, M, E)
-end
+## example 
 ##
 model = Model(
     optimizer_with_attributes(Ipopt.Optimizer,
     "max_wall_time" => 30.0)
 )
-orbparams_i = add_orbital_elements_fix!(model, true)
+orbparams_i = add_orbital_elements!(model, true)
 r, v, a, e, i, Ω, ω, nu, M, E = getfield.(Ref(orbparams_i), fieldnames(FullOrbitalParameters))
 
 # @constraint(model, a*(1-e) == rp)
