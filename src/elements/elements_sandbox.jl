697041ad5690e82c0574fafbbc1a2b3f09fb0f63
pekpuglia <pepuglia@gmail.com>
2025-04-20 11:34:42 +0200

    elements sandbox structure

--- src/elements/elements_sandbox.jl
+++ src/elements/elements_sandbox.jl
@@ -4,70 +4,59 @@ using JuMP
 using Ipopt
 using Setfield
 using ForwardDiff
-include("TG.jl")
+include("../TG.jl")
 using .TG
 using LinearAlgebra
 ## example
-orbi = KeplerianElements(
+orb = KeplerianElements(
     date_to_jd(2023, 1, 1, 0, 0, 0),
-    1.0000e+07,
+    9.6000e+06,
     0.0100,
-    0.1,
-    4.5379,
-    3.4907,
-    2.6180
+    0.6981,
+    0,
+    1.2217,
+    5.4105
 )
-ri, vi = kepler_to_rv(orbi)
-T = orbital_period(orbi, GM_EARTH)
-Δt = 1.4T
-plot_orbit(orbi)
+given_r, given_v = kepler_to_rv(orb)
+plot_orbit(orb)
 ##
 model = Model(
     optimizer_with_attributes(Ipopt.Optimizer,
     "max_wall_time" => 30.0)
 )
-orbparams_i = add_orbital_elements!(model)
-r0, v0, a0, e0, i0, Ω0, ω0, nu0, M0, E0 = getfield.(Ref(orbparams_i), fieldnames(FullOrbitalParameters))
+orbparams = add_orbital_elements!(model)
+r, v, a, e, i, Ω, ω, f, M, E = getfield.(Ref(orbparams), fieldnames(FullOrbitalParameters))
+        
+@constraint(model, r .== given_r)
+@constraint(model, v .== given_v)
 
-orbparams_f = add_orbital_elements!(model)
-rf, vf, af, ef, i_f, Ωf, ωf, nuf, Mf, Ef = getfield.(Ref(orbparams_f), fieldnames(FullOrbitalParameters))
-
-@constraint(model, r0 .== ri)
-@constraint(model, v0 .== vi)
-
-@constraint(model, af == a0)
-@constraint(model, ef == e0)
-@constraint(model, i_f == i0)
-@constraint(model, Ωf == Ω0)
-@constraint(model, ωf == ω0)
-
-@constraint(model, Δt == (Mf - M0) / (2π) * T)
+# @constraint(model, a == orb.a)
+# @constraint(model, e == orb.e)
+# @constraint(model, i == orb.i)
+# @constraint(model, Ω == orb.Ω)
+# @constraint(model, ω == orb.ω)
+# @constraint(model, f == orb.f)
 model
 ##
 optimize!(model)
 ##
-value(a0), value(af)
+value(a)
 ##
-value(e0), value(ef)
+value(e)
 ##
-value(i0), value(i_f)
+value(i)
 ##
 #oK!
-value(Ω0), value(Ωf)
+value(Ω)
 ##
-value(ω0), value(ωf)
+value(ω)
 ##
-value(nu0), value(nuf)
+value(f)
 ##
-solved_rf = value.(rf)
-solved_vf = value.(vf)
-
-solved_r0 = value.(r0)
-solved_v0 = value.(v0)
-
+solved_r = value.(r)
+solved_v = value.(v)
 
 plot_orbit(
-    orbi,
-    rv_to_kepler(solved_rf, solved_vf),
-    rv_to_kepler(solved_r0, solved_v0),
+    orb,
+    rv_to_kepler(solved_r, solved_v),
 )
