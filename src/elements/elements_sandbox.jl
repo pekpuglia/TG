d3c83dee7b937819dab91cfdc93ff4799188cf52
pekpuglia <pepuglia@gmail.com>
2025-04-21 12:22:53 +0200

    rescaling of r

--- src/elements/elements_sandbox.jl
+++ src/elements/elements_sandbox.jl
@@ -21,78 +21,66 @@ orb = KeplerianElements(
 given_r, given_v = kepler_to_rv(orb)
 plot_orbit(orb)
 ##
-function add_orbital_elements_fix!(model)
-    Vorb_sup = √(GM_EARTH/EARTH_EQUATORIAL_RADIUS)
-    rscaled = @variable(model, [1:3])
-    # @constraint(model, rscaled' * rscaled >= 1)
-    # radius_scaled = @variable(model, lower_bound = 1e-3, base_name = "radius")
-    # right_ascension = @variable(model, base_name="right_ascension")
-    # declination = @variable(model, lower_bound=-π/2, upper_bound=π/2, base_name="declination")
-
-    # rscaled = radius_scaled * [cos(right_ascension)*cos(declination); sin(right_ascension)*cos(declination); sin(declination)]
-
-    r = EARTH_EQUATORIAL_RADIUS * rscaled
-    vscaled = @variable(model, [1:3])
-    v = Vorb_sup*vscaled
-
-    ascaled = @variable(model, lower_bound = 1e-3)
-    a = EARTH_EQUATORIAL_RADIUS * ascaled
-    e = @variable(model, lower_bound = 0, upper_bound = 1)
-    i = @variable(model, lower_bound = 0, upper_bound = π, base_name = "i")
-    Ω = @variable(model, base_name = "Ω")
-    ω = @variable(model, base_name = "ω")
-    nu = @variable(model, lower_bound = -2π, upper_bound = 2π, base_name = "nu")
-
-    #rad!!!
-    M = @variable(model, lower_bound = 0.0, base_name = "M")
-    E = @variable(model, lower_bound = 0.0, base_name= "E")
-
-    R3Omega = [
-            cos(Ω) sin(Ω) 0
-        -sin(Ω) cos(Ω) 0
-        0          0        1
-    ]
-
-    R1i = [
-        1  0         0
-        0  cos(i) sin(i)
-        0 -sin(i) cos(i)
-    ]
-
-    R3omega = [
-        cos(ω)  sin(ω) 0
-        -sin(ω) cos(ω) 0
-        0          0        1
-    ]
-
-    QXxbar = R3omega * R1i * R3Omega
-
-    #curtis chap 4
-    #h^2/mu = p = a (1-e^2)
-    r_perifocal_scaled = ascaled*(1-e^2) * 1/(1+e*cos(nu)) * [cos(nu); sin(nu); 0]
-
-    #scaled with sqrt(GM_EARTH/EARTH_EQUATORIAL_RADIUS)
-    v_perifocal_scaled = 1 / √(ascaled*(1-e^2)) * [-sin(nu); e + cos(nu); 0]
-
-
-    @constraint(model, rscaled .== QXxbar' * r_perifocal_scaled)
-    @constraint(model, vscaled .== QXxbar' * v_perifocal_scaled)
-
-    @constraint(model, E - e*sin(E) == M)
-
-    #curtis page 144 & 145
-    @constraint(model, nu == 2 * atan(√(1+e)*sin(E/2), √(1-e)*cos(E/2)))
-            
-    rscaled, vscaled, a, e, i, Ω, ω, nu, M, E
-end
-##
 # function add_orbital_elements_fix!(model)
 model = Model(
     optimizer_with_attributes(Ipopt.Optimizer,
     "max_wall_time" => 30.0)
 )
 
-rscaled, vscaled, a, e, i, Ω, ω, nu, M, E = add_orbital_elements_fix!(model)
+Vorb_sup = √(GM_EARTH/EARTH_EQUATORIAL_RADIUS)
+rscaled = @variable(model, [1:3])
+@constraint(model, rscaled' * rscaled >= 1)
+r = EARTH_EQUATORIAL_RADIUS * rscaled
+vscaled = @variable(model, [1:3])
+v = Vorb_sup*vscaled
+
+ascaled = @variable(model, lower_bound = 1.0)
+a = EARTH_EQUATORIAL_RADIUS * ascaled
+e = @variable(model, lower_bound = 0, upper_bound = 1) 
+i = @variable(model, lower_bound = 0, upper_bound = π, base_name = "i")
+Ω = @variable(model, base_name = "Ω")
+ω = @variable(model, base_name = "ω")
+nu = @variable(model, lower_bound = -2π, upper_bound = 2π, base_name = "nu")
+
+#rad!!!
+M = @variable(model, lower_bound = 0.0, base_name = "M")
+E = @variable(model, lower_bound = 0.0, base_name= "E")
+
+R3Omega = [
+     cos(Ω) sin(Ω) 0
+    -sin(Ω) cos(Ω) 0
+    0          0        1
+]
+
+R1i = [
+    1  0         0
+    0  cos(i) sin(i)
+    0 -sin(i) cos(i)
+]
+
+R3omega = [
+    cos(ω)  sin(ω) 0
+    -sin(ω) cos(ω) 0
+    0          0        1
+]
+
+QXxbar = R3omega * R1i * R3Omega
+
+#curtis chap 4
+#h^2/mu = p = a (1-e^2)
+r_perifocal_scaled = ascaled*(1-e^2) * 1/(1+e*cos(nu)) * [cos(nu); sin(nu); 0]
+
+h = √(GM_EARTH*a*(1-e^2))
+v_perifocal = GM_EARTH / h * [-sin(nu); e + cos(nu); 0]
+
+
+@constraint(model, rscaled .== QXxbar' * r_perifocal_scaled)
+@constraint(model, v .== QXxbar' * v_perifocal)
+
+@constraint(model, E - e*sin(E) == M)
+
+#curtis page 144 & 145
+@constraint(model, nu == 2 * atan(√(1+e)*sin(E/2), √(1-e)*cos(E/2)))
 
 @constraint(model, rscaled .== (given_r/EARTH_EQUATORIAL_RADIUS))
 Vorb_sup = √(GM_EARTH/EARTH_EQUATORIAL_RADIUS)
