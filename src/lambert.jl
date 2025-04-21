e16b0973c1735bcc3a4c4d12d5ac3d8b2e3eb7c7
pekpuglia <pepuglia@gmail.com>
2025-04-21 16:05:53 +0200

    correct matrix and inverse

--- src/lambert.jl
+++ src/lambert.jl
@@ -87,7 +87,7 @@ function lambert(r1, r2, deltat)
 
     v1, v2
 end
-##
+## add test for this
 v1, v2 = lambert(r1, r2, delta_t)
 ##
 orb1 = rv_to_kepler(r1, v1)
@@ -95,59 +95,89 @@ orb2 = rv_to_kepler(r2, v2)
 ##
 plot_orbit(orb1, orb2)
 ##
-orb1 = rv_to_kepler(r1, v1)
-orb2 = rv_to_kepler(r2, v2)
-TG.plot_orbit(orb1, orb2)
-##
 #GLANDORF expressions
-R0 = r1
-V0 = v1
 
+g_glandorf = ((p/h)*(p+r)*r*sin(θ) - 3*e*p*delta_t)/(1-e^2)
+## R, V, r, theta funções do tempo
+#computes more than needed for P but ok
+function glandorf_precompute(R, V, t)
+    r = norm(R)
+
+    H = cross(R, V)
+    h = norm(H)
+    
+    orb = rv_to_kepler(R, V)
+
+    p = orb.a * (1 - orb.e^2)
+    θ = orb.f
+    e = orb.e
+
+    g_glandorf = ((p/h)*(p+r)*r*sin(θ) - 3*e*p*t)/(1-e^2)
+    f_glandorf = - (p/h) * (p+r)*r*cos(θ)
+
+    F3 = - (h/p)*sin(θ)
+    F4 = (h/p)*(e + cos(θ))
+    F5 = GM_EARTH / r^3
+
+    F = [
+        r*cos(θ)
+        r*sin(θ)
+        F3
+        F4
+        F5
+        3t
+        3GM_EARTH*t/r^3
+        F3 + g_glandorf*F5
+        F4 + f_glandorf*F5
+    ]
+
+    σ = cross(R, H)
+    w = cross(V, H)
+    b1 =    h + F[5]*(F[1]*f_glandorf - F[2]*g_glandorf)
+    b2 = F[6]*h + F[3]*f_glandorf - F[4]*g_glandorf
+    b3 = F[6]*h + 2*(F[3]*f_glandorf - F[4]*g_glandorf)
+    b4 = F[1]*f_glandorf - F[2]*g_glandorf
+
+    b = [b1; b2;b3;b4]
+
+    F, g_glandorf, f_glandorf, σ, w, b
+end
+##
 R = r2
 V = v2
-r = norm(R)
-
+H = cross(R, V)
 t = delta_t
-
-H = cross(R0, V0)
-h = norm(H)
-
-p = orb1.a * (1 - orb1.e^2)
-θ = orb2.f
-e = orb1.e
+F, g_glandorf, f_glandorf, σ, w, b = glandorf_precompute(R, V, t)
 ##
-g_glandorf = ((p/h)*(p+r)*r*sin(θ) - 3*e*p*delta_t)/(1-e^2)
-f_glandorf = - (p/h) * (p+r)*r*cos(θ)
-## R, V, r, theta funções do tempo
-F1 = r*cos(θ)
-F2 = r*sin(θ)
-F3 = - (h/p)*sin(θ)
-F4 = (h/p)*(e + cos(θ))
+function PRVt(R, V, t)
+    F, g_glandorf, f_glandorf, σ, w, b = glandorf_precompute(R, V, t)
+    H = cross(R, V)
+    
+    [
+        F[1]*R-g_glandorf*V F[2]*R-f_glandorf*V 2*R-F[6]*V  V    F[1]*H F[2]*H
+        F[8]*R-F[1]*V         F[9]*R-F[2]*V         F[7]*R-V   -F[5]*R F[3]*H F[4]*H
+    ]
+end
 ##
-F5 = GM_EARTH / r^3
-F6 = 3t
-F7 = 3GM_EARTH*t/r^3
-F8 = F3 + g_glandorf*F5
-F9 = F4 + f_glandorf*F5
+P = PRVt(R, V, t)
 ##
-P = [
-    F1*R-g_glandorf*V F2*R-f_glandorf*V 2*R-F6*V  V    F1*H F2*H
-    F8*R-F1*V         F9*R-F2*V         F7*R-V   -F5*R F3*H F4*H
-]
+function PinvRVt(R, V, t)
+    F, g_glandorf, f_glandorf, σ, w, b = glandorf_precompute(R, V, t)
+    H = cross(R, V)
+    h = norm(H)
+    
+    1/h^3 * [
+        F[2]*F[5]*σ' - F[4]*w'  2*F[4]*σ' - F[2]*w'
+        -F[1]*F[5]*σ' + F[3]*w' -2*F[3]*σ' + F[1]*w'
+        h*w'              -h*σ'
+        -b[1]*σ'+b[2]*w'      -b[3]*σ' + b[4]*w'
+        F[4]*H'             -F[2]*H'
+        -F[3]*H'             F[1]*H'
+    ]
+end
 ##
-σ = cross(R, H)
-w = cross(V, H)
-b1 =    h + F5*(F1*f_glandorf - F2*g_glandorf)
-b2 = F6*h + F3*f_glandorf - F4*g_glandorf
-b3 = F6*h + 2*(F3*f_glandorf - F4*g_glandorf)
-b4 = F1*f_glandorf - F2*g_glandorf
-Pinv = 1/h^3 * [
-     F2*F5*σ' - F4*w'  2*F4*σ' - F2*w'
-    -F1*F5*σ' + F3*w' -2*F3*σ' + F1*w'
-    h*w'              -h*σ'
-    -b1*σ'+b2*w'      -b3*σ' + b4*w'
-    F4*H'             -F2*H'
-    -F3*H'             F1*H'
-]
+Pinv = PinvRVt(R, V, t)
 ##
 P*Pinv
+##
+PRVt(r1, v1, 0)*PinvRVt(r1, v1, 0)
