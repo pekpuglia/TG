29fe046b3a65ded81d8642e30f61119159350fd7
pekpuglia <pepuglia@gmail.com>
2025-04-22 13:52:42 +0200

    p0 dot for N singular found

--- src/hohmann.jl
+++ src/hohmann.jl
@@ -5,7 +5,7 @@ using .TG
 using Setfield
 using LinearAlgebra
 ##
-extra_phase = 10
+extra_phase = 0
 orb1 = KeplerianElements(
     date_to_jd(2023, 1, 1, 0, 0, 0),
     7000e3,
@@ -13,7 +13,7 @@ orb1 = KeplerianElements(
     51 |> deg2rad,
     0    |> deg2rad,
     0     |> deg2rad,
-    -extra_phase     |> deg2rad
+    0     |> deg2rad
 )
 
 orb2 = KeplerianElements(
@@ -25,25 +25,33 @@ orb2 = KeplerianElements(
     0     |> deg2rad,
     180+extra_phase     |> deg2rad
 )
-## hohmann might cause N to be non invertible
-transfer_a = (orb1.a + orb2.a) / 2
+##
+hohmann_start_phase = extra_phase * 1
+hohmann_start = KeplerianElements(
+    date_to_jd(2023, 1, 1, 0, 0, 0),
+    (orb1.a + orb2.a) / 2,
+    (orb2.a - orb1.a) / (orb1.a + orb2.a),
+    orb1.i,
+    orb1.Ω,
+    orb1.ω + deg2rad(hohmann_start_phase),
+    0 |> deg2rad
+)
+hohmann_end = @set hohmann_start.f = π
+##
+plot_orbit(orb1, orb2, hohmann_start, hohmann_end)
+##
+hohmann_time = orbital_period(hohmann_start, GM_EARTH) / 2
 
-transfer_orbit_time = 2π√(transfer_a^3 / GM_EARTH) / 2
+initial_coast_time = max(hohmann_start_phase / 360 * orbital_period(orb1, GM_EARTH), 0)
+terminal_coast_time = max((extra_phase - hohmann_start_phase) / 360 * orbital_period(orb2, GM_EARTH), 0)
 
-min_transfer_time = transfer_orbit_time + extra_phase/360 * orbital_period(orb1, GM_EARTH)
-max_transfer_time = transfer_orbit_time + extra_phase/360 * orbital_period(orb2, GM_EARTH)
-transfer_time = max_transfer_time
+transfer_time = initial_coast_time + hohmann_time + terminal_coast_time
 ##
 r1, v1             = kepler_to_rv(orb1)
 r2, v2             = kepler_to_rv(orb2)
 vt_start, vt_end = lambert(r1, r2, transfer_time)
 ##
 plot_orbit(orb1, orb2, rv_to_kepler(r1, vt_start))
-##
-# r1, v1             = kepler_to_rv(orb1)
-# rt_start, vt_start = kepler_to_rv(transfer_start)
-# rt_end, vt_end     = kepler_to_rv(transfer_end)
-# r2, v2             = kepler_to_rv(orb2)
 ## conway 2.43 → onwards
 deltaV0 = vt_start - v1
 deltaVf = v2 - vt_end
@@ -57,7 +65,10 @@ N = Phi[1:3, 4:6]
 S = Phi[4:6, 1:3]
 T = Phi[4:6, 4:6]
 ##
-p0dot = N \ (pf - M * p0)
+# p0dot = N \ (pf - M * p0)
+A = N * [r1 vt_start]
+b = pf - M * p0
+p0dot = [r1 vt_start] * (A \ b)
 ## get Phi over time
 function Phi_time(propagator, t)
     r, v = Propagators.propagate!(propagator, t)
