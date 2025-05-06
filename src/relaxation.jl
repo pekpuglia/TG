1f1435f41fb7f3be63943563f2c37ae32f8af71e
pekpuglia <pepuglia@gmail.com>
2025-05-06 11:06:42 +0200

    turns

--- src/relaxation.jl
+++ src/relaxation.jl
@@ -7,6 +7,11 @@ using GLMakie
 using LinearAlgebra
 ##
 a1 = 7000e3
+a2 = 9000e3
+
+hohmann_time = orbital_period((a1+a2)/2, GM_EARTH) / 2
+
+transfer_time = hohmann_time
 
 orb1 = KeplerianElements(
     date_to_jd(2023, 1, 1, 0, 0, 0),
@@ -17,8 +22,19 @@ orb1 = KeplerianElements(
     0     |> deg2rad,
     60     |> deg2rad
 )
+
+orb2 = KeplerianElements(
+    orb1.t + transfer_time / 86400,
+    a2,
+    0.0,
+    orb1.i,
+    orb1.Ω,
+    orb1.ω,
+    deg2rad(180) + orb1.f
+)
 ##
 r1, v1 = kepler_to_rv(orb1)
+r2, v2 = kepler_to_rv(orb2)
 ##
 function two_body_dyn(X, mu)
     r = X[1:3]
@@ -49,19 +65,6 @@ function Xs(f, X0, dt, N, integrator=euler)
     sol
 end
 ##
-tmax = orbital_period(orb1, GM_EARTH)/2
-N = 10
-dt = tmax/N
-sol = Xs(X -> two_body_dyn(X, GM_EARTH), [r1;v1], dt, N, RK4)
-##
-r = getindex.(sol, Ref(1:3))
-r_history = hcat(r...)
-##
-scatter(r_history[1, :], r_history[2, :], r_history[3, :])
-##
-rf = r[end]
-
-##
 function add_coast_segment(model, deltat, N, ind)
     #[(x, y, z), time instants]
     r = @variable(model, [1:3, 1:N+1], base_name="r_coast_$ind")
@@ -80,20 +83,52 @@ function add_coast_segment(model, deltat, N, ind)
     r, v
 end
 ##
+N=10
 model = Model(Ipopt.Optimizer)
 
-r, v = add_coast_segment(model, tmax, N, 1)
+t1 = @variable(model, base_name="t1", lower_bound=0, upper_bound=transfer_time)
+t2 = @variable(model, base_name="t2", lower_bound=0, upper_bound=transfer_time)
+
+@constraint(model, t1+t2 <= transfer_time)
+
+deltaV1mag = @variable(model, lower_bound = 0, base_name="dV1mag")
+deltaV2mag = @variable(model, lower_bound = 0, base_name="dV2mag")
+
+deltaV1dir = @variable(model, [1:3], base_name="dV1dir")
+@constraint(model, deltaV1dir' * deltaV1dir == 1)
+deltaV2dir = @variable(model, [1:3], base_name="dV2dir")
+@constraint(model, deltaV2dir' * deltaV2dir == 1)
+
+
+rcoast1, vcoast1 = add_coast_segment(model, t1, N, 1)
+rcoast2, vcoast2 = add_coast_segment(model, t2, N, 2)
+rcoast3, vcoast3 = add_coast_segment(model, transfer_time-t1-t2, N, 3)
 
 for i = 1:(N+1)
-    set_start_value.(r[:, i], r1)
+    set_start_value.(rcoast1[:, i], r1)
+    set_start_value.(rcoast2[:, i], r1)
+    set_start_value.(rcoast3[:, i], r1)
 end
 
 
-@constraint(model, r[:, 1] .== r1)
-@constraint(model, r[:, end] .== rf)
+@constraint(model, rcoast1[:, 1] .== r1)
+
+@constraint(model, rcoast2[:, 1] .== rcoast1[:, end])
+deltaV1 = deltaV1mag * deltaV1dir
+@constraint(model, vcoast2[:, 1] .== vcoast1[:, end] + deltaV1)
+
+
+@constraint(model, rcoast3[:, 1] .== rcoast2[:, end])
+deltaV2 = deltaV2mag * deltaV2dir
+@constraint(model, vcoast3[:, 1] .== vcoast2[:, end] + deltaV2)
+
+
+@constraint(model, rcoast3[:, end] .== r2)
+
+@objective(model, MIN_SENSE, deltaV1mag + deltaV2mag)
 ##
 optimize!(model)
 ##
-solved_r = value.(r)
+solved_r = [value.(rcoast1) value.(rcoast2) value.(rcoast3)]
 ##
 scatter(solved_r[1, :], solved_r[2, :], solved_r[3, :])
