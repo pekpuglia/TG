36972dc07f8eed9b1d2c157b1b2dbd4936825a7e
pekpuglia <pepuglia@gmail.com>
2025-06-30 23:21:06 +0200

    refactor code

--- src/TG.jl
+++ src/TG.jl
@@ -30,7 +30,7 @@ function plot_orbit(orbs::KeplerianElements...)
     end
     
     wireframe!(ax3d, Sphere(Point3(0.0), EARTH_EQUATORIAL_RADIUS), color=:cyan, alpha=0.3)
-    fig
+    fig, ax3d
 end
 
 export orbital_period
@@ -40,305 +40,59 @@ end
 
 orbital_period(a, m0) = 2π√(a^3/m0)
 
-include("./lambert.jl")
+include("legacy.jl")
+
 include("./glandorf.jl")
-include("./coast_transfer.jl")
+include("integrators.jl")
 
-noNaNs(x::Real) = true
-noNaNs(x::ForwardDiff.Dual) = !any(isnan, ForwardDiff.partials(x))
-
-export propagate_coast
-function propagate_coast(xi, yi, zi, vxi, vyi, vzi, ti, deltat)
-    r = [xi; yi; zi]
-    v = [vxi; vyi; vzi]
-    
-    #always shows 0 at the beginning of solve - why?
-    (norm(r) < EARTH_EQUATORIAL_RADIUS) ? (@info "Passing through Earth r = $r") : nothing
-    Vesc = √(2tbc_m0 / EARTH_EQUATORIAL_RADIUS)
-    (norm(v) <= 100.0) ? (@info "Velocity very small v = $v") : nothing
-
-    # @assert norm(v) > 1e3 "Velocity too small"
-    # @assert norm(cross(r, v)) > 1e9 "Angular momentum too small"
-    energy = -tbc_m0/norm(r) + (v'*v)/2
-    @assert energy < 0 "Energy non negative: $energy, t = $ti, r = $r, v = $v"
-
-
-    orbi = rv_to_kepler([xi, yi, zi], [vxi, vyi, vzi], ti)
-
-    #low excentricity + f ≈ 0 causes NaN derivatives
-    @assert noNaNs(orbi.a) "NaN found"
-    @assert noNaNs(orbi.e) "NaN found"
-    @assert noNaNs(orbi.f) "NaN found"
-    @assert noNaNs(orbi.i) "NaN found"
-    @assert noNaNs(orbi.t) "NaN found"
-    @assert noNaNs(orbi.Ω) "NaN found"
-    @assert noNaNs(orbi.ω) "NaN found"
-
-    propagator = Propagators.init(Val(:TwoBody), orbi, propagation_type=Real)
-    r, v = Propagators.propagate!(propagator, deltat)
-
-    perigee = orbi.a * (1 - orbi.e)
-
-    [r; v; propagator.tbd.orbk.t; perigee]
-end
-
-export memoize
-# https://jump.dev/JuMP.jl/stable/tutorials/nonlinear/tips_and_tricks/#User-defined-operators-with-vector-outputs
-"""
-    memoize(foo::Function, n_outputs::Int)
-
-Take a function `foo` and return a vector of length `n_outputs`, where element
-`i` is a function that returns the equivalent of `foo(x...)[i]`.
-
-To avoid duplication of work, cache the most-recent evaluations of `foo`.
-Because `foo_i` is auto-differentiated with ForwardDiff, our cache needs to
-work when `x` is a `Float64` and a `ForwardDiff.Dual`.
-"""
-function memoize(foo::Function, n_outputs::Int)
-    last_x, last_f = nothing, nothing
-    last_dx, last_dfdx = nothing, nothing
-    function foo_i(i, x::T...) where {T<:Real}
-        if T == Float64
-            if x !== last_x
-                last_x, last_f = x, foo(x...)
-            end
-            return last_f[i]::T
-        else
-            if x !== last_dx
-                last_dx, last_dfdx = x, foo(x...)
-            end
-            return last_dfdx[i]::T
-        end
-    end
-    return [(x...) -> foo_i(i, x...) for i in 1:n_outputs]
-end
-
-export add_coast_operators!
-function add_coast_operators!(model)
-    memoized_propagate_coast = memoize(propagate_coast, 8)
-    
-    #for some reason this is required to avoid error no method matching getindex(::Nothing, ::Int64)
-    #no f clue why
-    #maybe something to do with the memoization initialization?
-    orb0 = KeplerianElements(
-    date_to_jd(2023, 1, 1, 0, 0, 0),
-    8000e3,
-    0.001111,
-    2 |> deg2rad,
-    50    |> deg2rad,
-    30     |> deg2rad,
-    1     |> deg2rad
-)
-    r0, v0 = kepler_to_rv(orb0)
-    ForwardDiff.gradient(x -> memoized_propagate_coast[7](x...), [r0..., v0..., 0.0, 10.0])
-
-    coast_position_x = @operator(model, coast_position_x, 8, memoized_propagate_coast[1])
-    coast_position_y = @operator(model, coast_position_y, 8, memoized_propagate_coast[2])
-    coast_position_z = @operator(model, coast_position_z, 8, memoized_propagate_coast[3])
-    coast_velocity_x = @operator(model, coast_velocity_x, 8, memoized_propagate_coast[4])
-    coast_velocity_y = @operator(model, coast_velocity_y, 8, memoized_propagate_coast[5])
-    coast_velocity_z = @operator(model, coast_velocity_z, 8, memoized_propagate_coast[6])
-    coast_time       = @operator(model, coast_time      , 8, memoized_propagate_coast[7])
-    coast_perigee    = @operator(model, coast_perigee   , 8, memoized_propagate_coast[8])
-
-    return (
-        (r, v, t, dt) -> [
-            coast_position_x(r..., v..., t, dt)
-            coast_position_y(r..., v..., t, dt)
-            coast_position_z(r..., v..., t, dt)
-        ],
-        (r, v, t, dt) -> [
-            coast_velocity_x(r..., v..., t, dt)
-            coast_velocity_y(r..., v..., t, dt)
-            coast_velocity_z(r..., v..., t, dt)
-        ],
-        (r, v, t, dt) -> coast_time(r..., v..., t, dt),
-        (r, v, t, dt) -> coast_perigee(r..., v..., t, dt)
-    )
-end
-
-export single_maneuver_model
-function single_maneuver_model(orb0, r_final, dir_v_final, total_time)
-    ## auxiliary parameters
-    Vesc = √(2tbc_m0 / EARTH_EQUATORIAL_RADIUS)
-    Vmin = 100.0
-
-    moon_distance = 384400.e2 
-
-    model = Model(
-        optimizer_with_attributes(Ipopt.Optimizer,
-            "max_iter" => 10000,
-            "max_wall_time" => 30.0)
-    )
-
-    #control variables
-    Δt_maneuver = @variable(model, Δt_maneuver, start=0.5*total_time)
-    @constraint(model, 0 <= Δt_maneuver <= total_time)  #set start value of constraints?
-
-    #colocar no referencial local da v_pre_maneuver
-    ΔVmag = @variable(model, 0 <= ΔVmag <= Vesc, start=1.0)
-    
-    ΔVdir = @variable(model, -1 <= ΔVdir[1:3] <= 1, start = 0.0)
-    set_start_value(ΔVdir[1], 1.0)
-
-    @constraint(model, ΔVdir' * ΔVdir == 1)
-
-    # ΔV = @variable(model, -Vesc <= ΔV[1:3] <= Vesc, start = 1.0)
-
-    coast_r, coast_v, coast_t, coast_perigee = add_coast_operators!(model)
-    
-    r0, v0 = kepler_to_rv(orb0)
-
-    #first coast
-    r_maneuver = coast_r(r0, v0, orb0.t, Δt_maneuver)
-
-    @constraint(model, EARTH_EQUATORIAL_RADIUS^2 <= r_maneuver' * r_maneuver <= moon_distance^2)
-
-    v_pre_maneuver = coast_v(r0, v0, orb0.t, Δt_maneuver)
-
-    @constraints(model, begin
-        Vesc >= v_pre_maneuver[1] >= -Vesc
-        Vesc >= v_pre_maneuver[2] >= -Vesc 
-        Vesc >= v_pre_maneuver[3] >= -Vesc 
-    end)
-    @constraint(model, Vesc^2 >= v_pre_maneuver' * v_pre_maneuver >= Vmin^2)
-    @constraint(model, (v_pre_maneuver' * v_pre_maneuver) * √(r_maneuver' * r_maneuver) / (2tbc_m0) <= 1-1e-6)
-
-    time_maneuver = coast_t(r0, v0, orb0.t, Δt_maneuver)
-    
-    #delta V is in tangential reference frame
-    #x ∥ v
-    #z ∥ h = r × v
-    #y = z × x
-
-    x_tang = v_pre_maneuver ./ √(v_pre_maneuver' * v_pre_maneuver)
-
-    h = cross(r_maneuver, v_pre_maneuver)
-
-    z_tang = h ./ √(h' * h)
-
-    y_tang = cross(z_tang, x_tang)
-
-    ΔV = ΔVmag * ΔVdir
-
-    v_post_maneuver = v_pre_maneuver + [x_tang y_tang z_tang] * ΔV
-
-    @constraints(model, begin
-        Vesc >= v_post_maneuver[1] >= -Vesc
-        Vesc >= v_post_maneuver[2] >= -Vesc 
-        Vesc >= v_post_maneuver[3] >= -Vesc 
-    end)
-    @constraint(model, Vesc^2 >= v_post_maneuver' * v_post_maneuver >= Vmin^2)
-    @constraint(model, (v_post_maneuver' * v_post_maneuver) * √(r_maneuver' * r_maneuver) / (2tbc_m0) <= 1-1e-6)
-    
-    #perigee outside of earth
-    @constraint(model, coast_perigee(r_maneuver, v_post_maneuver, time_maneuver, total_time - Δt_maneuver) >= EARTH_EQUATORIAL_RADIUS)
-
-    #second coast
-    rf = coast_r(r_maneuver, v_post_maneuver, time_maneuver, total_time - Δt_maneuver)
-
-    @constraint(model, EARTH_EQUATORIAL_RADIUS^2 <= rf' * rf <= moon_distance^2)
-
-    vf = coast_v(r_maneuver, v_post_maneuver, time_maneuver, total_time - Δt_maneuver)
-    
-    @constraints(model, begin
-        Vesc >= vf[1] >= -Vesc
-        Vesc >= vf[2] >= -Vesc 
-        Vesc >= vf[3] >= -Vesc 
-    end)
-    @constraint(model, Vesc^2 >= vf' * vf >= Vmin^2)
-    @constraint(model, (vf' * vf) * √(rf' * rf) / (2tbc_m0) <= 1-1e-6)
-
-    dir_vf = vf ./ √(vf' * vf)
-    
-    @constraints(model, begin
-        -100.0 <= rf[1] - r_final[1] <= 100.0
-        -100.0 <= rf[2] - r_final[2] <= 100.0
-        -100.0 <= rf[3] - r_final[3] <= 100.0
-        dot(dir_vf, dir_v_final) >= 0.97
-    end)
-    
-    # @objective(model, MIN_SENSE, √(ΔV' * ΔV))
-    @objective(model, MIN_SENSE, ΔVmag)
-
-    model, r_maneuver, v_post_maneuver, rf, vf
-end
-
-#testing purposes only
-export final_position
-function final_position(orb0, deltaV, period_fraction1, period_fraction2)
-    T0 = orbital_period(orb0, tbc_m0)
-
-    r_preman, v_preman, orbp_preman = Propagators.propagate(Val(:TwoBody), period_fraction1*T0, orb0)
-    orb_preman = orbp_preman.tbd.orbk
-
-    v_postman = v_preman + deltaV
-    orb_postman = rv_to_kepler(r_preman, v_postman, orb_preman.t)
-    T_postman = orbital_period(orb_postman, tbc_m0)
-
-    r_final, v_final, orbp_postman = Propagators.propagate(Val(:TwoBody), period_fraction2*T_postman, orb_postman)
-    total_time = orbp_preman.tbd.Δt + orbp_postman.tbd.Δt
-    r_final, total_time, orb_postman, orbp_postman.tbd.orbk, v_final
-end
-
-export FullOrbitalParameters
-struct FullOrbitalParameters
-    r
-    v
-    a
-    e
-    i
-    Ω
-    ω
-    nu
-    M
-    E # excluir?
-end
-
-export add_orbital_elements!
-function add_orbital_elements!(model)
-    Vorb_sup = √(GM_EARTH/EARTH_EQUATORIAL_RADIUS)
-    rscaled = @variable(model, [1:3])
-    @constraint(model, rscaled' * rscaled >= 1)
-    r = EARTH_EQUATORIAL_RADIUS * rscaled
-    vscaled = @variable(model, [1:3])
-    v = Vorb_sup*vscaled
-
-    ascaled = @variable(model, lower_bound = 1.0)
-    a = EARTH_EQUATORIAL_RADIUS * ascaled
-    e = @variable(model, lower_bound = 0, upper_bound = 1) 
-    i = @variable(model, lower_bound = 0, upper_bound = π, base_name = "i")
-    Ω = @variable(model, base_name = "Ω")
-    ω = @variable(model, base_name = "ω")
-    nu = @variable(model, lower_bound = -2π, upper_bound = 2π, base_name = "nu")
-
-    #rad!!!
-    M = @variable(model, lower_bound = 0.0, base_name = "M")
-    E = @variable(model, lower_bound = 0.0, base_name= "E")
-    
-    QxbarX = [
-        -sin(Ω)*cos(i)*sin(ω)+cos(Ω)*cos(ω) -sin(Ω)*cos(i)*cos(ω)-cos(Ω)*sin(ω)  sin(Ω)*sin(i)
-         cos(Ω)*cos(i)*sin(ω)+sin(Ω)*cos(ω)  cos(Ω)*cos(i)*cos(ω)-sin(Ω)*sin(ω) -cos(Ω)*sin(i)
-         sin(i)*sin(ω)                               sin(i)*cos(ω)                              cos(i)
+export two_body_dyn
+function two_body_dyn(X, mu)
+    r = X[1:3]
+    v = X[4:6]
+    [
+        v
+        -mu/(√(r'*r)^3)*r
     ]
+end
 
-    #curtis chap 4
-    #h^2/mu = p = a (1-e^2)
-    r_perifocal = a*(1-e^2) * 1/(1+e*cos(nu)) * [cos(nu); sin(nu); 0]
-    
-    h = √(GM_EARTH*a*(1-e^2))
-    v_perifocal = GM_EARTH / h * [-sin(nu); e + cos(nu); 0]
+export add_coast_segment
+function add_coast_segment(model, deltat, N, ind; dyn=(X -> two_body_dyn(X, GM_EARTH)), integrator=RK4)
+    #[(x, y, z), time instants]
+    r = @variable(model, [1:3, 1:N+1], base_name="r_coast_$ind")
+    v = @variable(model, [1:3, 1:N+1], base_name="v_coast_$ind")
 
 
-    @constraint(model, r .== QxbarX * r_perifocal)
-    @constraint(model, v .== QxbarX * v_perifocal)
-    
-    @constraint(model, E - e*sin(E) == M)
+    for i = 1:(N+1)
+        # @constraint(model, r[:, i]' * r[:, i] >= EARTH_EQUATORIAL_RADIUS^2)
+        # @constraint(model, cross(r[:, i], v[:, i])[3] >=0)
+    end
 
-    #curtis page 144 & 145
-    @constraint(model, nu == 2 * atan(√(1+e)*sin(E/2), √(1-e)*cos(E/2)))
+    for i = 1:N
+        @constraint(model, integrator(dyn, [r[:, i]; v[:, i]], deltat/N) .== [r[:, i+1]; v[:, i+1]])
+    end
 
-    FullOrbitalParameters(r, v, a, e, i, Ω, ω, nu, M, E)
+    r, v
+end
+
+export p0dot_tpbvp
+function p0dot_tpbvp(p0, pf, delta_t, prop)
+    Phi = TG.Phi_time(prop, delta_t)
+
+    M = Phi[1:3, 1:3]
+    N = Phi[1:3, 4:6]
+
+    if abs(det(N)) <= 1e-10
+        #CHECK THIS IS TRUE
+        #only works for coplanar transfers?
+        r1, v1 = kepler_to_rv(prop.tbd.orb₀)
+        A = N * [r1 v1]
+        b = pf - M * p0
+        p0dot = [r1 v1] * (A \ b)
+    else
+        p0dot = N \ (pf - M * p0)
+    end
+
+    p0dot
 end
 
 end # module TG
--- src/relaxation.jl
+++ src/relaxation.jl
@@ -7,34 +7,12 @@ using GLMakie
 using LinearAlgebra
 using Setfield
 ##
-function plot_orbit_fix(orbs::KeplerianElements...)
-    N = 100
-    θ = LinRange(0, 2π, N)
-
-    fig = Figure()
-    ax3d = Axis3(fig[1, 1])
-    
-    for (orb, c) in zip(orbs, Makie.wong_colors())
-        orbit = ((@set orb.f = theta) for theta in θ) .|> kepler_to_rv .|> first |> stack
-        current_pos, current_velocity = kepler_to_rv(orb)
-        velocity_arrow_base = current_pos
-        velocity_arrow_tip = velocity_arrow_base + current_velocity / √sum(current_velocity .^ 2) * 0.4 * √sum(current_pos .^ 2)
-        velocity_arrow_data = [velocity_arrow_base velocity_arrow_tip] |> Matrix
-        lines!(ax3d, orbit[1, :], orbit[2, :], orbit[3, :], color=c)
-        scatter!(ax3d, current_pos, markersize=20, color=c)
-        lines!(ax3d, velocity_arrow_data[1, :], velocity_arrow_data[2, :], velocity_arrow_data[3, :], color=c)
-    end
-    
-    wireframe!(ax3d, Sphere(Point3(0.0), EARTH_EQUATORIAL_RADIUS), color=:cyan, alpha=0.3)
-    fig, ax3d
-end
-##
 a1 = 7000e3
-a2 = 9000e3
+a2 = 40000e3
 
 hohmann_time = orbital_period((a1+a2)/2, GM_EARTH) / 2
 
-transfer_time = hohmann_time
+transfer_time = 2hohmann_time
 
 orb1 = KeplerianElements(
     date_to_jd(2023, 1, 1, 0, 0, 0),
@@ -50,9 +28,9 @@ prop1 = Propagators.init(Val(:TwoBody), orb1)
 orb2 = KeplerianElements(
     orb1.t + transfer_time / 86400,
     a2,
-    0.0,
+    0.2,
     orb1.i,
-    orb1.Ω,
+    orb1.Ω + deg2rad(31),
     orb1.ω,
     deg2rad(180) + orb1.f
 )
@@ -64,11 +42,12 @@ hohmann_orb = KeplerianElements(
     orb1.i,
     orb1.Ω,
     orb1.ω,
+
     deg2rad(60)
 )
 
 prop2 = Propagators.init(Val(:TwoBody), orb2)
-f, ax3d = plot_orbit_fix(orb1, orb2, hohmann_orb)
+f, ax3d = plot_orbit(orb1, orb2, hohmann_orb)
 f
 ##
 save("./report/img/hohmann_condition.png", f)
@@ -79,241 +58,10 @@ save("./report/img/hohmann_condition_in_plane.png", f)
 ##
 r1, v1 = kepler_to_rv(orb1)
 r2, v2 = kepler_to_rv(orb2)
-##
-function two_body_dyn(X, mu)
-    r = X[1:3]
-    v = X[4:6]
-    [
-        v
-        -mu/(√(r'*r)^3)*r
-    ]
-end
 
-function euler(f, X, dt)
-    X + f(X)*dt
-end
-
-function RK4(f, X, dt)
-    k1 = dt*f(X)
-    k2 = dt*f(X+k1/2)
-    k3 = dt*f(X+k2/2)
-    k4 = dt*f(X+k3)
-    X + (k1+2k2+2k3+k4)/6
-end
-
-function RK8(f, X, dt)
-    c = zeros(Float64, (16,))
-    c[1] =  0
-    c[2] =  .4e-1
-    c[3] =  .9648736013787361245235039379666356743708e-1
-    c[4] =  .1447310402068104186785255906949953511556
-    c[5] =  .576
-    c[6] =  .2272326564618766017153738192188229509142
-    c[7] =  .5407673435381233982846261807811770490858
-    c[8] =  .64
-    c[9] =  .48
-    c[10] =  .6754e-1
-    c[11] =  .25
-    c[12] =  .6770920153543242682384311058159603931192
-    c[13] =  .8115
-    c[14] =  .906
-    c[15] =  1
-    c[16] =  1
-
-    a = zeros(Float64, (16,16))
-    a[2,1] =  .4e-1
-
-    a[3,1] = -.198852731918229097650241511466089129345e-1
-    a[3,2] =  .1163726333296965222173745449432724803716
-
-    a[4,1] =  .3618276005170260466963139767374883778890e-1
-    a[4,2] =  0
-    a[4,3] =  .1085482801551078140088941930212465133667
-
-    a[5,1] =  2.272114264290177409193144938921415409241
-    a[5,2] =  0
-    a[5,3] = -8.526886447976398578316416192982602292786
-    a[5,4] =  6.830772183686221169123271254061186883545
-
-    a[6,1] =  .5094385535389374394512668566783434123978e-1
-    a[6,2] =  0
-    a[6,3] =  0
-    a[6,4] =  .1755865049809071110203693328749561646990
-    a[6,5] =  .70229612707574674987780067603244497535e-3
-
-    a[7,1] =  .1424783668683284782770955365543878809824
-    a[7,2] =  0
-    a[7,3] =  0
-    a[7,4] = -.3541799434668684104094753917518523845155
-    a[7,5] =  .7595315450295100889001534202778550159932e-1
-    a[7,6] =  .6765157656337123215269906939508560510196
-
-    a[8,1] =  .7111111111111111111111111111111111111111e-1
-    a[8,2] =  0
-    a[8,3] =  0
-    a[8,4] =  0
-    a[8,5] =  0
-    a[8,6] =  .3279909287605898328568406057725491803016
-    a[8,7] =  .2408979601282990560320482831163397085872
-
-    a[9,1] =  .7125e-1
-    a[9,2] =  0
-    a[9,3] =  0
-    a[9,4] =  0
-    a[9,5] =  0
-    a[9,6] =  .3268842451575245554847578757216915662785
-    a[9,7] =  .1156157548424754445152421242783084337215
-    a[9,8] = -.3375e-1
-
-    a[10,1] =  .4822677322465810178387112087673611111111e-1
-    a[10,2] =  0
-    a[10,3] =  0
-    a[10,4] =  0
-    a[10,5] =  0
-    a[10,6] =  .3948559980495400110769549704186108167677e-1
-    a[10,7] =  .1058851161934658144373823566907778072121
-    a[10,8] = -.2152006320474309346664428710937500000000e-1
-    a[10,9] = -.1045374260183348238623046875000000000000
-
-    a[11,1] = -.2609113435754923412210928689962011065179e-1
-    a[11,2] =  0
-    a[11,3] =  0
-    a[11,4] =  0
-    a[11,5] =  0
-    a[11,6] =  .3333333333333333333333333333333333333333e-1
-    a[11,7] = -.1652504006638105086724681598195267241410
-    a[11,8] =  .3434664118368616658319419895678838776647e-1
-    a[11,9] =  .1595758283215209043195814910843067811951
-    a[11,10] =  .2140857321828193385584684233447183324979
-
-    a[12,1] = -.362842339625565859076509979091267105528e-1
-    a[12,2] =  0
-    a[12,3] =  0
-    a[12,4] =  0
-    a[12,5] =  0
-    a[12,6] = -1.096167597427208807028761474420297770752
-    a[12,7] =  .1826035504321331052308236240517254331348
-    a[12,8] =  .708225444417068325613028685455625123741e-1
-    a[12,9] = -.231364701848243126999929738482630407146e-1
-    a[12,10] =  .2711204726320932916455631550463654973432
-    a[12,11] =  1.308133749422980744437146904349994472286
-
-    a[13,1] = -.5074635056416974879347823927726392374259
-    a[13,2] =  0
-    a[13,3] =  0
-    a[13,4] =  0
-    a[13,5] =  0
-    a[13,6] = -6.631342198657237090355284142048733580937
-    a[13,7] = -.252748010090880105270020973014860316405
-    a[13,8] = -.4952612380036095562991116175550167835424
-    a[13,9] =  .293252554525388690285739720360003594753
-    a[13,10] =  1.440108693768280908474851998204423941413
-    a[13,11] =  6.237934498647055877243623886838802127716
-    a[13,12] =  .7270192054526987638549835199880202544289
-
-    a[14,1] =  .6130118256955931701496387847232542148725
-    a[14,2] =  0
-    a[14,3] =  0
-    a[14,4] =  0
-    a[14,5] =  0
-    a[14,6] =  9.088803891640463313341034206647776279557
-    a[14,7] = -.407378815629344868103315381138325162923
-    a[14,8] =  1.790733389490374687043894756399015035977
-    a[14,9] =  .714927166761755073724875250629602731782
-    a[14,10] = -1.438580857841722850237810322456327208949
-    a[14,11] = -8.263329312064740580595954649844133476994
-    a[14,12] = -1.537570570808865115231450725068826856201
-    a[14,13] =  .3453832827564871699090880801079644428793
-
-    a[15,1] = -1.211697910343873872490625222495537087293
-    a[15,2] =  0
-    a[15,3] =  0
-    a[15,4] =  0
-    a[15,5] =  0
-    a[15,6] = -19.05581871559595277753334676575234493500
-    a[15,7] =  1.26306067538987510135943101851905310045
-    a[15,8] = -6.913916969178458046793476128409110926069
-    a[15,9] = -.676462266509498065300115641383621209887
-    a[15,10] =  3.367860445026607887090352785684064242560
-    a[15,11] =  18.00675164312590810020103216906571965203
-    a[15,12] =  6.838828926794279896350389904990814350968
-    a[15,13] = -1.031516451921950498420447675652291096155
-    a[15,14] =  .4129106232130622755368055554332539084021
-
-    a[16,1] =  2.157389007494053627033175177985666660692
-    a[16,2] =  0
-    a[16,3] =  0
-    a[16,4] =  0
-    a[16,5] =  0
-    a[16,6] =  23.80712219809580523172312179815279712750
-    a[16,7] =  .88627792492165554903036801415266308369
-    a[16,8] =  13.13913039759876381480201677314222971522
-    a[16,9] = -2.604415709287714883747369630937415176632
-    a[16,10] = -5.193859949783872300189266203049579105962
-    a[16,11] = -20.41234071154150778768154893536134356354
-    a[16,12] = -12.30085625250572261314889445241581039623
-    a[16,13] =  1.521553095008539362178397458330791655267
-    a[16,14] =  0
-    a[16,15] =  0
-
-    b = zeros(Float64, (16,))
-    b[1] =  .1458885278405539719101539582255752917034e-1
-    b[2] =  0
-    b[3] =  0
-    b[4] =  0
-    b[5] =  0
-    b[6] =  0
-    b[7] =  0
-    b[8] =  .2024197887889332650566666683195656097825e-2
-    b[9] =  .2178047084569716646796256135839225745895
-    b[10] =  .1274895340854389692868677968654808668201
-    b[11] =  .2244617745463131861258531547137348031621
-    b[12] =  .1787254491259903095100090833796054447157
-    b[13] =  .7594344758096557172908303416513173076283e-1
-    b[14] =  .1294845879197561516869001434704642286297
-    b[15] =  .2947744761261941714007911131590716605202e-1
-    b[16] =  0
-
-    F = zeros(Float64, (length(X),16))
-
-    for i = 1:16
-        Y = X + dt * sum(a[i, 1:(i-1)]' .* F[:, 1:(i-1)], dims=2)
-        F[:, i] = f(Y)
-    end
-
-    X + dt*sum(b' .* F, dims=2)
-end
-#test with xdot(x) = x
-# RK8(xdot, 1, 1) = e
-
-function Xs(f, X0, dt, N, integrator=euler)
-    sol = [X0]
-    for i = 1:N
-        push!(sol, integrator(f, sol[i], dt))
-    end
-    sol
-end
-##
-function add_coast_segment(model, deltat, N, ind)
-    #[(x, y, z), time instants]
-    r = @variable(model, [1:3, 1:N+1], base_name="r_coast_$ind")
-    v = @variable(model, [1:3, 1:N+1], base_name="v_coast_$ind")
-
-
-    for i = 1:(N+1)
-        # @constraint(model, r[:, i]' * r[:, i] >= EARTH_EQUATORIAL_RADIUS^2)
-        @constraint(model, cross(r[:, i], v[:, i])[3] >=0)
-    end
-
-    for i = 1:N
-        @constraint(model, RK4(X -> two_body_dyn(X, GM_EARTH), [r[:, i]; v[:, i]], deltat/N) .== [r[:, i+1]; v[:, i+1]])
-    end
-
-    r, v
-end
 ##
 #solve lambert problem to use as initial condition
-N = 20
+N = 100
 
 model = Model(Ipopt.Optimizer)
 
@@ -339,7 +87,7 @@ optimize!(model)
 ##
 lamb1 = rv_to_kepler(value.(r[:, 1]), value.(v[:, 1]))
 lamb_prop = Propagators.init(Val(:TwoBody), lamb1)
-f, ax3d = plot_orbit_fix(orb1, orb2, lamb1)
+f, ax3d = plot_orbit(orb1, orb2, lamb1)
 f
 # save("./report/img/hohmann_lambert_guess.png", f)
 ##
@@ -439,10 +187,10 @@ solved_r = [value.(rcoast1) value.(rcoast2) value.(rcoast3)]
 # f = Figure()
 # ax3d = Axis3(f[1, 1])
 
-f, ax3d = plot_orbit_fix(orb1, orb2)
+f, ax3d = plot_orbit(orb1, orb2)
 
 scatter!(ax3d, solved_r[1, :], solved_r[2, :], solved_r[3, :], color="green")
-scatter!(ax3d, solved_r[1, (N+1):(N+1):end], solved_r[2, (N+1):(N+1):end], solved_r[3, (N+1):(N+1):end], markersize=20, color=:red)
+scatter!(ax3d, solved_r[1, (N+1):(N+1):end-1], solved_r[2, (N+1):(N+1):end-1], solved_r[3, (N+1):(N+1):end-1], markersize=20, color=:red)
 wireframe!(ax3d, Sphere(Point3(0.0), EARTH_EQUATORIAL_RADIUS), color=:cyan, alpha=0.3)
 
 f
@@ -452,27 +200,16 @@ ax3d.azimuth = -π/2
 ax3d.elevation = orb1.i
 save("./report/img/hohmann_solved_in_plane.png", f)
 ##
-plot_orbit(orb1, orb2, transf_orb1, transf_orb2, transf_orb3)
+f, _ = plot_orbit(orb1, orb2, transf_orb1, transf_orb2, transf_orb3)
+f
 ## primer vector 
 
 p0 = deltaV1_solved / norm(deltaV1_solved)
 pf = deltaV2_solved / norm(deltaV2_solved)
 
 inter_impulse_prop = Propagators.init(Val(:TwoBody), transf_orb2)
-Phi = TG.Phi_time(inter_impulse_prop, value(dt2))
 
-M = Phi[1:3, 1:3]
-N = Phi[1:3, 4:6]
-
-if abs(det(N)) <= 1e-10
-    #CHECK THIS IS TRUE
-    #only works for coplanar transfers?
-    A = N * [r1 v1]
-    b = pf - M * p0
-    p0dot = [r1 v1] * (A \ b)
-else
-    p0dot = N \ (pf - M * p0)
-end
+p0dot = p0dot_tpbvp(p0, pf, value(dt2), inter_impulse_prop)
 ## plot p and pdot
 tspan = range(0, value(dt2), 100)
 ##
@@ -486,5 +223,5 @@ lines!(ax1, tspan, norm.(getindex.(ppdot, Ref(1:3))))
 
 ax2 = Axis(f[2, 1], xlabel = "t (s)", ylabel = L"d |p| / dt")
 lines!(ax2, tspan, normpdot)
-
+f
 # save("./report/img/hohmann_primer_vector_history.png", f)
