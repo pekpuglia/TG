92b91a913134d5f81a534ac6ded15d8b954aca9f
pekpuglia <pepuglia@gmail.com>
2025-07-28 16:32:00 +0200

    add transfer struct

--- src/interactive_pv.jl
+++ src/interactive_pv.jl
@@ -20,6 +20,50 @@ function initial_guess_initorb!(orb1, tf, r, v)
         set_start_value.(v[:, i], vguess)
     end
 end
+
+struct Transfer
+    X1::Vector
+    X2::Vector
+    mu::Float64
+    transfer_time
+    rcoast::Array
+    vcoast::Array
+    deltaVmags::Vector
+    deltaVdirs::Matrix
+    dts::Vector
+end
+
+struct ScaledTransfer
+    transfer::Transfer
+    L #always include?
+    T
+end
+
+#scale options?
+function lambert_transfer_model(X1, X2, deltat, MU, N)
+    r1 = X1[1:3]
+    v1 = X1[4:6]
+    r2 = X2[1:3]
+    v2 = X2[4:6]
+
+    model = Model(Ipopt.Optimizer)
+
+    r, v = add_coast_segment(model, deltat, N, "")
+
+    dV = @variable(model, [1:2], lower_bound=0)
+    dVdirs = @variable(model, [1:3, 1:2])
+    @constraint(model, [i=1:2], dVdirs[:, i]' * dVdirs[:, i] == 1)
+
+    @constraint(model, r[:, 1] .== r1)
+    @constraint(model, r[:, end] .== r2)
+
+    @constraint(model, dV[1] * dVdirs[:, 1] == v[:, 1] - v1)
+    @constraint(model, dV[2] * dVdirs[:, 2] == (v[:, end] - v2))
+
+    @objective(model, MIN_SENSE, sum(dV))
+
+    model, Transfer(X1, X2, MU, deltat, r, v, dV, dVdirs, [deltat])
+end
 ##
 case_ind = 1
 orb1, orb2 = ORBIT_STARTS[case_ind], ORBIT_ENDS[case_ind]
@@ -30,24 +74,14 @@ tf1 = orbital_period((orb1.a+orb2.a)/2, GM_EARTH) / 2
 
 model = Model(Ipopt.Optimizer)
 
-r, v = add_coast_segment(model, tf1, 200, "")
+model, model_transfer = lambert_transfer_model([r1; v1], [r2; v2], tf1, GM_EARTH, 100)
 
-dV = @variable(model, [1:2], lower_bound=0)
-
-@constraint(model, r[:, 1] .== r1)
-@constraint(model, r[:, end] .== r2)
-
-@constraint(model, dV[1]^2 == (v[:, 1] - v1)' * (v[:, 1] - v1))
-@constraint(model, dV[2]^2 == (v[:, end] - v2)' * (v[:, end] - v2))
-
-@objective(model, MIN_SENSE, sum(dV))
-
-initial_guess_initorb!(orb1, tf1, r, v)
+initial_guess_initorb!(orb1, tf1, model_transfer.rcoast, model_transfer.vcoast)
 ##
 optimize!(model)
 ##
-solved_r = value.(r)
-solved_v = value.(v)
+solved_r = value.(model_transfer.rcoast)
+solved_v = value.(model_transfer.vcoast)
 solved_orb = rv_to_kepler(solved_r[:, 1], solved_v[:, 1])
 solved_prop = Propagators.init(Val(:TwoBody), solved_orb)
 ##
@@ -76,3 +110,4 @@ vlines!(ax2, tf1, linestyle=:dash, color=:gray)
 f
 ##
 save("results/"*PREFIXES[case_ind]*"_primer_vector.png", f)
+##
