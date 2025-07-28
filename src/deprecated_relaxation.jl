0ebb8e08024a13767007cc4576e3f69c349fb63c
pekpuglia <pepuglia@gmail.com>
2025-07-28 15:15:38 +0200

    added normalization

--- src/deprecated_relaxation.jl
+++ src/deprecated_relaxation.jl
@@ -2,7 +2,8 @@ using SatelliteToolboxBase
 using SatelliteToolboxPropagators
 using JuMP
 using Ipopt
-using TG
+include("TG.jl")
+using .TG
 using GLMakie
 using LinearAlgebra
 using Setfield
@@ -20,6 +21,12 @@ hohmann_time = orbital_period((a1+a2)/2, GM_EARTH) / 2
 
 transfer_time = 2hohmann_time
 
+L = (a1+a2)/2
+T = transfer_time
+
+MUPRIME = T^2 * GM_EARTH / L ^ 3
+
+##
 orb1 = KeplerianElements(
     date_to_jd(2023, 1, 1, 0, 0, 0),
     a1,
@@ -65,13 +72,19 @@ save("./report/img/hohmann_condition_in_plane.png", f)
 r1, v1 = kepler_to_rv(orb1)
 r2, v2 = kepler_to_rv(orb2)
 
+r1 = r1 / L
+v1 = v1 * T / L
+
+r2 = r2 / L
+v2 = v2 * T / L
 ##
 #solve lambert problem to use as initial condition
 N = 100
 
 model = Model(Ipopt.Optimizer)
 
-r, v = add_coast_segment(model, transfer_time, N, "lamb")
+r, v = add_coast_segment(model, transfer_time / T, N, "lamb",
+    dyn=(X -> two_body_dyn(X, MUPRIME)))
 
 dt10 = 0/3*transfer_time
 dt20 = 3/3*transfer_time
@@ -79,19 +92,19 @@ dt20 = 3/3*transfer_time
 r1_lamb, v1_lamb = Propagators.propagate!(prop1, dt10)
 r2_lamb = Propagators.propagate!(prop2, dt10+dt20-transfer_time)[1]
 
-@constraint(model, r[:, 1] .== r1_lamb)
-@constraint(model, r[:, end] .== r2_lamb)
+@constraint(model, r[:, 1] .== r1_lamb / L)
+@constraint(model, r[:, end] .== r2_lamb / L)
 
 #follow natural orbit
 for i = 1:size(r)[2]
     rlamb_guess, vlamb_guess = Propagators.propagate!(prop1, dt10 + dt20 * (i/(N+1)))
-    set_start_value.(r[:, i], rlamb_guess)
-    set_start_value.(v[:, i], vlamb_guess)
+    set_start_value.(r[:, i], rlamb_guess / L)
+    set_start_value.(v[:, i], vlamb_guess * T / L)
 end
 ##
 optimize!(model)
 ##
-lamb1 = rv_to_kepler(value.(r[:, 1]), value.(v[:, 1]))
+lamb1 = rv_to_kepler(L * value.(r[:, 1]), L/T * value.(v[:, 1]))
 lamb_prop = Propagators.init(Val(:TwoBody), lamb1)
 f, ax3d = plot_orbit(orb1, orb2, lamb1)
 f
@@ -116,8 +129,8 @@ model = Model(
 # dt3 = @variable(model, base_name="dt3", lower_bound=0, upper_bound=transfer_time)
 # @constraint(model, dt1 + dt2 + dt3 == transfer_time)
 
-dts = @variable(model, [1:nimp+1], base_name = "dt", lower_bound=0, upper_bound=transfer_time)
-@constraint(model, sum(dts) == transfer_time)
+dts = @variable(model, [1:nimp+1], base_name = "dt", lower_bound=0, upper_bound=transfer_time / T)
+@constraint(model, sum(dts) == transfer_time / T)
 
 # deltaV1mag = @variable(model, lower_bound = 0, base_name="dV1mag")
 # deltaV2mag = @variable(model, lower_bound = 0, base_name="dV2mag")
@@ -135,7 +148,9 @@ deltaVdirs = @variable(model, [1:3, 1:nimp], base_name = "dVdir")
 # rcoast2, vcoast2 = add_coast_segment(model, dt2, N, 2)
 # rcoast3, vcoast3 = add_coast_segment(model, dt3, N, 3)
 
-rvcoasts = [add_coast_segment(model, dts[i], N, i) for i = 1:nimp+1]
+rvcoasts = [add_coast_segment(
+    model, dts[i], N, i,
+    dyn=(X -> two_body_dyn(X, MUPRIME))) for i = 1:nimp+1]
 
 rcoast = cat(first.(rvcoasts)..., dims=3)
 vcoast = cat(last.(rvcoasts)..., dims=3)
@@ -167,7 +182,7 @@ end
 
 #start condition - set to maneuvers along lambert between start and end
 #lambert maneuver
-set_start_value.(dts, [dt10; dt20; transfer_time-dt10-dt20])
+set_start_value.(dts, [dt10; dt20; transfer_time-dt10-dt20] / T)
 # set_start_value(dt2, dt20)
 # set_start_value(dt3, transfer_time - dt10 - dt20)
 
@@ -181,16 +196,16 @@ set_start_value.(deltaVdirs[2], dv2/norm(dv2))
 
 for i = 1:(N+1)
     r1i, v1i = Propagators.propagate!(prop1, dt10*(i-1)/N)
-    set_start_value.(rcoast[:, i, 1], r1i)
-    set_start_value.(vcoast[:, i, 1], v1i)
+    set_start_value.(rcoast[:, i, 1], r1i / L)
+    set_start_value.(vcoast[:, i, 1], v1i * T / L)
     
     r2i, v2i = Propagators.propagate!(lamb_prop, dt20*(i-1)/N)
-    set_start_value.(rcoast[:, i, 2], r2i)
-    set_start_value.(vcoast[:, i, 2], v2i)
+    set_start_value.(rcoast[:, i, 2], r2i / L)
+    set_start_value.(vcoast[:, i, 2], v2i * T / L)
     
     r3i, v3i = Propagators.propagate!(prop2, (transfer_time-dt10-dt20)*((i-1)/N - 1))
-    set_start_value.(rcoast[:, i, 3], r3i)
-    set_start_value.(vcoast[:, i, 3], v3i)
+    set_start_value.(rcoast[:, i, 3], r3i / L)
+    set_start_value.(vcoast[:, i, 3], v3i * T / L)
 end
 ##
 optimize!(model)
@@ -200,11 +215,11 @@ deltaVdir_solved = value.(deltaVdirs)
 
 deltaV_solved = deltaVdir_solved .* deltaVmag_solved'
 ##
-deltaVmodel = objective_value(model)
+deltaVmodel = objective_value(model) * L / T
 ##
-transf_orb1 = rv_to_kepler(value.(rcoast[:, 1, 1]), value.(vcoast[:, 1, 1]))
-transf_orb2 = rv_to_kepler(value.(rcoast[:, 1, 2]), value.(vcoast[:, 1, 2]))
-transf_orb3 = rv_to_kepler(value.(rcoast[:, 1, 3]), value.(vcoast[:, 1, 3]))
+transf_orb1 = rv_to_kepler(L * value.(rcoast[:, 1, 1]), L / T * value.(vcoast[:, 1, 1]))
+transf_orb2 = rv_to_kepler(L * value.(rcoast[:, 1, 2]), L / T * value.(vcoast[:, 1, 2]))
+transf_orb3 = rv_to_kepler(L * value.(rcoast[:, 1, 3]), L / T * value.(vcoast[:, 1, 3]))
 ##
 v1n = norm(v1)
 v2n = norm(v2)
@@ -212,7 +227,7 @@ vtransf1 = √(2*GM_EARTH*(1/a1 - 1/(a1+a2)))
 vtransf2 = √(2*GM_EARTH*(1/a2 - 1/(a1+a2)))
 deltaV_hohmann = v2n - vtransf2 + vtransf1 - v1n
 ##
-solved_r = [value.(rcoast[:, :, 1]) value.(rcoast[:, :, 2]) value.(rcoast[:, :, 3])]
+solved_r = L * [value.(rcoast[:, :, 1]) value.(rcoast[:, :, 2]) value.(rcoast[:, :, 3])]
 ##
 # f = Figure()
 # ax3d = Axis3(f[1, 1])
