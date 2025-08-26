58cfbb42d7fa3d285d9625b4fba64b745847259d
pekpuglia <pepuglia@gmail.com>
2025-08-26 10:29:32 +0200

    very good trajectory, very bad optimum !?

--- src/interactive_pv.jl
+++ src/interactive_pv.jl
@@ -58,13 +58,13 @@ function initial_orb_sequence(orb1, tf, Ndisc, nimp::Int, init_coast::Bool, fina
     sequence
 end
 ##
-case_ind = 5
+case_ind = 3
 orb1, orb2 = ORBIT_STARTS[case_ind], ORBIT_ENDS[case_ind]
 r1, v1 = kepler_to_rv(orb1)
 r2, v2 = kepler_to_rv(orb2)
 
 #vary tf for each orbit
-tf_real = orbital_period((orb1.a+orb2.a)/2, GM_EARTH) / 2
+tf_real = orbital_period((orb1.a+orb2.a)/2, GM_EARTH)
 
 L = (orb1.a+orb2.a)/2
 T = 1
@@ -78,9 +78,10 @@ X1 = [r1 / L; v1 * T / L]
 X2 = [r2 / L; v2 * T / L]
 
 N = 100
-planner, transfer = n_impulse_transfer(X1, X2, tfprime, MUPRIME, N, 2, false, false)
 ##
-solver = casadi.nlpsol("S", "ipopt", planner.prob)
+planner, transfer = n_impulse_transfer(X1, X2, tfprime, MUPRIME, N, 2, false, false);
+##
+solver = casadi.nlpsol("S", "ipopt", planner.prob);
 ## initial guess
 
 seq0 = [scale(s, L, T) 
@@ -113,7 +114,7 @@ save_with_views!(ax3d, f, "results/$(PREFIXES[case_ind])")
 
 
 #automate this - discard early departure/late arrival
-tspan_ppdot = primer_vector(solved_model, 1000, planar=true)
+tspan_ppdot = primer_vector(solved_model, 1000)
 tspan, ppdot = tspan_ppdot[1]
 normp = norm.(getindex.(ppdot, Ref(1:3)))
 normpdot = [dot(ppdoti[1:3], ppdoti[4:6]) / norm(ppdoti[1:3]) for ppdoti in ppdot]
@@ -132,26 +133,20 @@ f
 save("results/"*PREFIXES[case_ind]*"_primer_vector.png", f)
 ##
 # try free impulse time solutions
-model = Model(optimizer_with_attributes(Ipopt.Optimizer,
-"max_iter" => 3_000,
-# "max_wall_time" => 30.0
-))
+N = 50
+planner, transfer = n_impulse_transfer(X1, X2, tfprime, MUPRIME, N, 2, true, true)
 
-transfer_time = tf1 #1.5 tf1 gives good orbit for case GEO
-model, model_transfer = n_impulse_model(model, X1, X2, transfer_time / T, MUPRIME, 100, 2, true, true)
-
-all_r = cat((model_transfer.sequence[i].rcoast for i = [1, 3, 5])..., dims=2)
-all_v = cat((model_transfer.sequence[i].vcoast for i = [1, 3, 5])..., dims=2)
-
-initial_guess_initorb!(all_r, all_v, orb1, transfer_time, L, T)
-
-# #false! need to concatenate initial condition segments
-# initial_guess_initorb!(model_transfer.sequence[3].rcoast, model_transfer.sequence[3].vcoast, orb1, tf1, L, T)
-# initial_guess_initorb!(model_transfer.sequence[5].rcoast, model_transfer.sequence[5].vcoast, orb1, tf1/2  , L, T)
-
-optimize!(model)
+solver = casadi.nlpsol("s", "ipopt", planner.prob)
 ##
-solved_model = unscale(solved(model_transfer), L, T)
+seq0 = [scale(s, L, T) 
+    for s = initial_orb_sequence(orb1, tf_real, N, 2, true, true, [0.5, 0, 0.5])
+]
+
+tab0 = vcat(varlist.(seq0)...)
+##
+sol = solve_planner(solver, planner, tab0)
+##
+solved_model = unscale(sol_to_transfer(sol, transfer), L, T)
 solved_orbs = [rv_to_kepler(c.rcoast[:, 1], c.vcoast[:, 1]) for c in solved_model.sequence if c isa Coast]
 # solved_prop = Propagators.init(Val(:TwoBody), solved_orb)
 ##
