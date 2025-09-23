e404ad8dfbdb42c927d3fb2f31685e1d57719d51
pekpuglia <pepuglia@gmail.com>
2025-09-23 22:24:16 +0200

    paper data was wrong

--- src/paper_non_cop_rdv.jl
+++ src/paper_non_cop_rdv.jl
@@ -8,7 +8,7 @@ include("casadi_transfer_model.jl")
 ##
 orb1 = KeplerianElements(
     date_to_jd(2023, 1, 1, 0, 0, 0),
-    6741.1e3,
+    6748.1e3,
     0.0,
     42.1 |> deg2rad,
     120.2    |> deg2rad,
@@ -18,7 +18,7 @@ orb1 = KeplerianElements(
 
 orb2_0 = KeplerianElements(
     orb1.t,
-    6771.1e3,
+    6778.1e3,
     0.0,
     42 |> deg2rad,
     120 |> deg2rad,
@@ -89,16 +89,17 @@ X1 = [r1 / L; v1 * T / L]
 X2 = [r2 / L; v2 * T / L]
 orbm = scale(orb_model, L, T)
 
-N = 100
-planner_2f, transfer_2f = n_impulse_transfer(orbm, X1, X2, tfprime, N, 2, true, true)
+N_2 = 100
+planner_2f, transfer_2f = n_impulse_transfer(orbm, X1, X2, tfprime, N_2, 2, true, true)
 
 solver_2f = casadi.nlpsol("s", "ipopt", planner_2f.prob, Dict("ipopt" => Dict(
     "max_iter" => 3000,
     "constr_viol_tol" => 1e-5,
     "max_wall_time" => 60)))
-##
+## 0.1 0.1 0.8 - 67
+# 0.1 0.8 0.1 - 55
 seq0 = [scale(s, L, T) 
-    for s = initial_orb_sequence(orb1, tf_real, N, 2, true, true, [0.8, 0.1, 0.1])
+    for s = initial_orb_sequence(orb1, tf_real, N_2, 2, true, true, [0.1, 0.8, 0.1])
 ]
 
 tab0 = vcat(varlist.(seq0)...)
@@ -106,93 +107,42 @@ tab0 = vcat(varlist.(seq0)...)
 sol = solve_planner(solver_2f, planner_2f, tab0)
 
 solved_transfer_2f = unscale(sol_to_transfer(sol, transfer_2f), L, T)
-total_dV(solved_transfer_2f)
+tdv2 = total_dV(solved_transfer_2f)
 ##
 f, ax3d = plot_orbit(orb1, orb2)
-add_transfer!(ax3d, solved_transfer, 1e3)
+add_transfer!(ax3d, solved_transfer_2f, 1e3)
 f
 ## impulse times
-tspan_ppdot = primer_vector(solved_transfer, PVTMGlandorf(), 100)
-plot_primer_vector(solved_transfer, tspan_ppdot)[1]
+tspan_ppdot_2f = primer_vector(solved_transfer_2f, PVTMGlandorf(), 100)
+plot_primer_vector(solved_transfer_2f, tspan_ppdot_2f)[1]
 ## 3 imp - use last solution
 #do j2 case!!!
-N = 50
-planner_3, transfer_3 = n_impulse_transfer(orbm, X1, X2, tfprime, N, 3, true, true)
+N_3 = 50
+planner_3, transfer_3 = n_impulse_transfer(orbm, X1, X2, tfprime, N_3, 3, true, true)
+
+add_inequality!(planner_3, sum(getfield.(impulses(transfer_3), :deltaVmag)), 0, tdv2 * T / L)
 
 solver_3 = casadi.nlpsol("s", "ipopt", planner_3.prob, Dict("ipopt" => Dict(
     "max_iter" => 3000,
     "constr_viol_tol" => 1e-5,
-    "max_wall_time" => 60)))
+    "max_wall_time" => 180)))
 ## add null impulse
 seq0 = [scale(s, L, T) 
-    for s = initial_orb_sequence(orb1, tf_real, N, 3, true, true, [0.5, 0.1, 0.2, 0.1])
+    for s = initial_orb_sequence(orb1, tf_real, N_3, 3, true, true, [0.5, 0.1, 0.2, 0.1])
 ]
 
+## init cond from sol 2
+transfer_2f_redisc = rediscretize_transfer(solved_transfer_2f, N_3)
+
+null_imp_transf_2 = scale(
+    add_null_impulse(
+        transfer_2f_redisc, 
+        tspan_ppdot_2f), L, T)
+
+seq0 = null_imp_transf_2.sequence
+##
 tab0 = vcat(varlist.(seq0)...)
-##
-N = 50 #change N later
-tspan, ppdot = tspan_ppdot
 
-# normpdot = [dot(ppdoti[1:3], ppdoti[4:6]) / norm(ppdoti[1:3]) for ppdoti in eachcol(ppdot)]
-
-normp = norm.(eachcol(ppdot[1:3, :]))
-
-max_norm_time_ind = findall(
-    (prev_el_next) -> prev_el_next[1] <= prev_el_next[2] && prev_el_next[2] >= prev_el_next[3], 
-    collect(zip(normp[1:end-2], normp[2:end-1], normp[3:end]))) .+ 1
-
-max_norm_time_ind = max_norm_time_ind[findmax(i -> normp[i], max_norm_time_ind)[2]]
-
-max_norm_time = tspan[max_norm_time_ind]
-#find where to insert impulse
-imp_ts = impulse_times(solved_transfer)
-#last impulse before maxnorm
-#if = 0, new impulse comes at the beginning
-new_impulse_ind = something(findlast(<=(max_norm_time), imp_ts), 0)
-
-#coast to split index
-impulse_indices = findall(el -> el isa Impulse, solved_transfer.sequence)
-
-coast_to_split_sequence_index = (new_impulse_ind == 0) ? 1 : (impulse_indices.+1)[new_impulse_ind]
-
-coast_to_split = solved_transfer.sequence[coast_to_split_sequence_index]
-
-delta_t_split = max_norm_time - [0; imp_ts][new_impulse_ind+1]
-
-xcoast_before = zeros(6, N)
-
-xcoast_before[:, 1] = [coast_to_split.rcoast[:, 1]; coast_to_split.vcoast[:, 1]]
-
-for i = 2:N
-    xcoast_before[:, i] =  RK8(X -> dynamics(X, solved_transfer.model), xcoast_before[:, i-1], delta_t_split / (N-1))
-end
-
-delta_t_after = coast_to_split.dt - delta_t_split
-
-xcoast_after = zeros(6, N)
-
-xcoast_after[:, 1] = xcoast_before[:, end]
-
-for i = 2:N
-    xcoast_after[:, i] =  RK8(X -> dynamics(X, solved_transfer.model), xcoast_after[:, i-1], delta_t_after / (N-1))
-end
-
-coast_before = Coast(xcoast_before[1:3, :], xcoast_before[4:6, :], delta_t_split)
-coast_after = Coast(xcoast_after[1:3, :], xcoast_after[4:6, :], delta_t_after)
-
-
-new_impulse = Impulse(0.0, (x -> x / norm(x))(ppdot[1:3, max_norm_time_ind]))
-
-new_seq = [solved_transfer.sequence[1:coast_to_split_sequence_index-1]; coast_before; new_impulse; coast_after; solved_transfer.sequence[coast_to_split_sequence_index+1:end]]
-new_transfer = Transfer(solved_transfer.X1, solved_transfer.X2, solved_transfer.model, solved_transfer.transfer_time, new_seq)
-
-new_transfer_scaled = scale(new_transfer, L, T)
-tab0 = vcat(varlist.(new_transfer_scaled.sequence)...)
-##
-f, ax3d = plot_orbit(orb1, orb2)
-add_transfer!(ax3d, new_transfer, 1e3)
-f
-##
 sol = solve_planner(solver_3, planner_3, tab0)
 
 solved_transfer = unscale(sol_to_transfer(sol, transfer_3), L, T)
