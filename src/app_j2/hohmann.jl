c36fa2a81f1d3c363921d0405c3b60e2c1b23283
pekpuglia <pepuglia@gmail.com>
2025-10-23 14:21:39 +0200

    j2 circle to circle

--- src/app_j2/hohmann.jl
+++ src/app_j2/hohmann.jl
@@ -32,27 +32,28 @@ tab0 = vcat(varlist.(seq0)...)
 ##
 sol = solve_planner(solver, planner, tab0)
 ##
-solved_transfer = unscale(sol_to_transfer(sol, transfer), L, T)
+solved_transfer2r = unscale(sol_to_transfer(sol, transfer), L, T)
 ##
 f, ax3d, orb_lines = plot_orbit(HOHMANN_START, HOHMANN_END)
 imp_scale = 1e3
-coast_ps, ia = add_transfer!(ax3d, solved_transfer, imp_scale)
+coast_ps, ia = add_transfer!(ax3d, solved_transfer2r, imp_scale)
 Legend(f[1, 2], [orb_lines..., coast_ps[1], ia[1]], ["Initial orbit", "Final orbit", "Coasting arc", "Impulse (×$imp_scale s)"], position = (0.8, 0.9))
 f
 ##
 save_with_views!(ax3d, f, "results/j2/hohmann/ICI")
 ##
-tspan_ppdot_stm = primer_vector(solved_transfer, PVTMFromSTM(100, RK8), 100)
-tspan_ppdot_ode = primer_vector(solved_transfer, PVTMFromODE(100, RK8), 100)
+tspan_ppdot_stm_2r = primer_vector(solved_transfer2r, PVTMFromSTM(100, RK8), 100)
+tspan_ppdot_ode = primer_vector(solved_transfer2r, PVTMFromODE(100, RK8), 100)
 ##
-f, axs = plot_primer_vector(solved_transfer, tspan_ppdot_stm, name=NAME, label="STM", style=:dash)
-plot_primer_vector!(f, axs[1], axs[2], solved_transfer, tspan_ppdot_ode, label="ODE", style=:dashdotdot)
+f, axs = plot_primer_vector(solved_transfer2r, tspan_ppdot_stm_2r, name=NAME, label="STM", style=:dash)
+plot_primer_vector!(f, axs[1], axs[2], solved_transfer2r, tspan_ppdot_ode, label="ODE", style=:dashdotdot)
 Legend(f[1, 2], axs[1], "Methods")
 f
 ##
 save("./results/j2/hohmann/ICI_primer_vector.png", f, px_per_unit = 300/96)
 ## data summary
-export_transfer(solved_transfer, tspan_ppdot_stm, L, T, tolepsilon)
+hohmann_tab = transfer_table(solved_transfer2r, tspan_ppdot_stm_2r, L, T, tolepsilon, NAME)
+dump_table("./report/text/resultados.tex", "% J2 C2C ICI", hohmann_tab)
 ###########################################################################################################################################
 ###########################################################################################################################################
 ###########################################################################################################################################
@@ -73,27 +74,28 @@ seq0 = [scale(s, L, T)
 
 tab0 = vcat(varlist.(seq0)...)
 sol = solve_planner(solver, planner, tab0)
-solved_transfer_2 = unscale(sol_to_transfer(sol, transfer), L, T)
+solved_transfer2f = unscale(sol_to_transfer(sol, transfer), L, T)
 ##
 f, ax3d, orb_lines = plot_orbit(HOHMANN_START, HOHMANN_END)
 imp_scale = 1e4
-coast_ps, ia = add_transfer!(ax3d, solved_transfer_2, imp_scale)
+coast_ps, ia = add_transfer!(ax3d, solved_transfer2f, imp_scale)
 Legend(f[1, 2], [orb_lines..., coast_ps[1], ia[1]], ["Initial orbit", "Final orbit", "Coasting arc", "Impulse (×$imp_scale s)"], position = (0.8, 0.9))
 f
 ##
-save_with_views!(ax3d, f, "results/j2/hohmann/$(transfer_type(solved_transfer_2))")
+save_with_views!(ax3d, f, "results/j2/hohmann/$(transfer_type(solved_transfer2f))")
 ##
-tspan_ppdot_stm_2 = primer_vector(solved_transfer_2, PVTMFromSTM(100, RK8), 100)
-tspan_ppdot_ode_2 = primer_vector(solved_transfer_2, PVTMFromODE(100, RK8), 100)
+tspan_ppdot_stm_2f = primer_vector(solved_transfer2f, PVTMFromSTM(100, RK8), 100)
+tspan_ppdot_ode = primer_vector(solved_transfer2f, PVTMFromODE(100, RK8), 100)
 ##
-f, axs = plot_primer_vector(solved_transfer_2, tspan_ppdot_stm_2, name=NAME, label="STM", style=:dash)
-plot_primer_vector!(f, axs[1], axs[2], solved_transfer_2, tspan_ppdot_ode_2, label="ODE", style=:dashdotdot)
+f, axs = plot_primer_vector(solved_transfer2f, tspan_ppdot_stm_2f, name=NAME, label="STM", style=:dash)
+plot_primer_vector!(f, axs[1], axs[2], solved_transfer2f, tspan_ppdot_ode, label="ODE", style=:dashdotdot)
 Legend(f[1, 2], axs[1], "Methods")
 f
 ##
-save("./results/j2/hohmann/$(transfer_type(solved_transfer_2))_primer_vector.png", f, px_per_unit = 300/96)
+save("./results/j2/hohmann/$(transfer_type(solved_transfer2f))_primer_vector.png", f, px_per_unit = 300/96)
 ## data summary
-export_transfer(solved_transfer_2, tspan_ppdot_stm_2, L, T, tolepsilon)
+hohmann_tab = transfer_table(solved_transfer2f, tspan_ppdot_stm_2f, L, T, tolepsilon, NAME)
+dump_table("./report/text/resultados.tex", "% J2 C2C 2 CICIC", hohmann_tab)
 ###########################################################################################################################################
 ###########################################################################################################################################
 ###########################################################################################################################################
@@ -107,7 +109,7 @@ solver = casadi.nlpsol("S", "ipopt", planner.prob, Dict("ipopt" => Dict(
     "max_iter" => 3000,
     "constr_viol_tol" => 1e-5)));
 ##
-null_transfer_2 = add_null_impulse(solved_transfer_2, tspan_ppdot_stm_2)
+null_transfer_2 = add_null_impulse(solved_transfer2f, tspan_ppdot_stm_2f)
 
 tab0 = vcat(varlist.(null_transfer_2.sequence)...)
 sol = solve_planner(solver, planner, tab0)
@@ -131,4 +133,8 @@ f
 ##
 save("./results/j2/hohmann/$(transfer_type(solved_transfer_3))_primer_vector.png", f, px_per_unit = 300/96)
 ## data summary
-export_transfer(solved_transfer_3, tspan_ppdot_stm_3, L, T, tolepsilon)
+hohmann_tab = transfer_table(solved_transfer_3, tspan_ppdot_stm_3, L, T, tolepsilon, NAME)
+dump_table("./report/text/resultados.tex", "% J2 C2C 3 CICICIC", hohmann_tab)
+##
+sum_tab = summary_table(NAME, solved_transfer2r, solved_transfer2f, solved_transfer_3)
+dump_table("./report/text/resultados.tex", "% J2 C2C SUM", sum_tab)
